ea48ed01f72e4a930189e851dad98d326c46d1c1
Terry Ligocki <tjligocki@gmail.com>
2005-10-08 18:14:49 -0400

    Copied "busyBeaverC.c" code over to new name and set up compiling.  This will eventually be the code that runs the same Turing machine at two different speeds (one twice as fast as the other) to detect infinite loops...

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ module1 = Extension('busyBeaverC',
 
 setup(name='busyBeaverC',
       version='1.0',
-      description='C speedups to busyBeaver',
+      description='C speedups to Turing machine simulation',
       ext_modules=[module1])
+
+module2 = Extension('twoMachineC',
+                    sources=['twoMachineC.c'],
+                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+
+setup(name='twoMachineC',
+      version='1.0',
+      description='Run one TM at two different speeds',
+      ext_modules=[module2])
--- setup_win.py
+++ setup_win.py
@@ -7,5 +7,13 @@ module1 = Extension('busyBeaverC',
 
 setup(name='busyBeaverC',
       version='1.0',
-      description='C speedups to busyBeaver',
+      description='C speedups to Turing machine simulation',
       ext_modules=[module1])
+
+module2 = Extension('twoMachineC',
+                    sources=['twoMachineC.c'])
+
+setup(name='twoMachineC',
+      version='1.0',
+      description='Run one TM at two different speeds',
+      ext_modules=[module2])
