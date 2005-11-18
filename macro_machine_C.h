0a2472f1bb0274c9537b4ff97365e38ee76fc70a
Terry Ligocki <tjligocki@gmail.com>
2005-11-18 11:38:49 -0500

    A new header file...

---
+++ macro_machine_C.h
@@ -0,0 +1,85 @@
+#include <Python.h>
+
+#define PRIME_HASH_NUMBER   100003
+
+typedef struct
+{
+  int w;
+  int d;
+  int s;
+} TRANSITION;
+
+typedef struct
+{
+  TRANSITION* t;
+} STATE;
+
+typedef struct
+{
+  int num_states;
+  int num_symbols;
+
+  STATE* machine;
+
+  int* tape;
+  int  tape_length;
+
+  int symbol;
+
+  int                total_symbols;
+  unsigned long long total_steps;
+
+  int position;
+  int max_left;
+  int max_right;
+
+  int state;
+
+  int new_symbol;
+  int new_delta;
+  int new_state;
+} TM;
+
+typedef struct macro_transition MACRO_TRANSITION;
+
+struct macro_transition
+{
+  int index;
+
+  int w;
+  int d;
+  int s;
+
+  MACRO_TRANSITION* next;
+};
+
+typedef struct
+{
+  MACRO_TRANSITION** t;
+} MACRO_STATE;
+
+typedef struct
+{
+  int num_states;
+  int num_symbols;
+
+  MACRO_STATE* machine;
+
+  int* tape;
+  int  tape_length;
+
+  int symbol;
+
+  int                total_symbols;
+  unsigned long long total_steps;
+
+  int position;
+  int max_left;
+  int max_right;
+
+  int state;
+
+  int new_symbol;
+  int new_delta;
+  int new_state;
+} MACRO_TM;
