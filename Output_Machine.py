9ab03c2a2d34626d6ed65bdc6d12c2ee432dfea2
sligocki <sligocki@localhost>
2006-08-15 10:17:46 -0400

    Little tool used for convering not very readable list of machines into a very human readable one allong with the # of ones and steps.

---
+++ Output_Machine.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import string
+
+def get_ttable(string):
+  start = string.find("[[")
+  end = string.rfind("]]") + len("]]")
+  return eval(string[start:end])
+
+def display_ttable(table):
+  symbols = string.digits
+  dirs = "LR"
+  states = string.ascii_uppercase[:7]
+  s = ""
+  for row in table:
+    for cell in row:
+      if cell[2] == -1:
+        s += "1RH "
+      else:
+        s += "%c%c%c " % (symbols[cell[0]], dirs[cell[1]], states[cell[2]])
+    s += " "
+  return s
+
+# Main script
+import sys
+
+filename = sys.argv[1]
+if filename == "-":
+  infile = sys.stdin
+else:
+  infile = open(filename, "r")
+
+for line in infile:
+  parts = line.split()
+  ones = int(parts[6])
+  steps = int(parts[7]) + 1
+  table = get_ttable(line)
+  print display_ttable(table), ones, "", steps
