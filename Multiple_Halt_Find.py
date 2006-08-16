eaba0fcf860db674b7ef30b33ae6434cddae40ec
sligocki <sligocki@localhost>
2006-08-16 06:57:15 -0400

    Updated with percentages and total number of machines.

--- Multiple_Halt_Find.py
+++ Multiple_Halt_Find.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+from __future__ import division
+
 def get_ttable(string):
   start = string.find("[[")
   end = string.rfind("]]") + len("]]")
@@ -14,7 +16,7 @@ def count_halts(ttable):
   return halts
 
 # Main
-import sys
+import sys, math
 
 infile = open(sys.argv[1], "r")
 
@@ -28,6 +30,10 @@ for line in infile:
     print line,
 
 # Output stats
+total_count = sum(count.values())
+width = int(math.log(total_count, 10)) + 2
 print
 for n in range(max(count.keys()) + 1):
-  print "Machines with %d halts: %d" % (n, count.get(n, 0))
+  print ("%"+`width`+"d machines with %d halts (%.1f%%)") % (count.get(n, 0), n, count.get(n, 0) * 100 / total_count)
+print
+print ("%"+`width`+"d total machines") % total_count
