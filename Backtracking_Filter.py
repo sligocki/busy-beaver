b30527908897c8eb19468bc3fb32322827590f63
sligocki <sligocki@localhost>
2006-08-17 08:24:48 -0400

    Welcome to the new backtracking filter!
    Catches about half of the remaining 2-symbol machines!
    Catches none of the remaining 2-state machines!
    
    We should try it on the 3x3's
    
    requires argument --backsteps= (number of steps to exprtapolate back)
    optional argument [--limit=1000] (max number of machines to have before
    giving up)
    
    I've been using --backstep=20, but of 615 caught by filter in 4x2:
      1 needed --backstep=11
      9 needed  8
      1 needed  7
     16 needed  6
      6 needed  5
     76         4
    116         3
    390         2
      1         1
    
    So, 8 might generally be sufficient.
    
    -limit could be much lower 100 should be fine, 20 would probably not
    loose any info, 10 might even do quite well.

---
+++ Backtracking_Filter.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python
+#
+# Filters out machines whose halt states obviously cannot be reached based
+# on reverse engineering/backtracking.
+
+from __future__ import division
+import IO, copy
+
+# Constants
+INFINITE = 4
+BACKTRACK = 5
+HALT_STATE = -1
+
+def init_array(val, num):
+  temp = [None] * num
+  for i in range(num):
+    temp[i] = val[:]
+  return temp
+
+def get_stats(TTable):
+  """Finds all halt transitions and other statistical info"""
+  num_states = len(TTable)
+  num_symbols = len(TTable[0])
+  halts = []
+  to_state = init_array([], num_states)
+  to_symbol = init_array([], num_symbols)
+  for state in range(num_states):
+    for symbol in range(num_symbols):
+      cell = TTable[state][symbol]
+      if cell[2] == HALT_STATE:
+        halts.append((state, symbol))
+      else:
+        to_state[cell[2]].append(((state, symbol), cell))
+        to_symbol[cell[0]].append(((state, symbol), cell))
+  return halts, to_state, to_symbol
+
+class Partial_Config:
+  def __init__(self, state, symbol):
+    self.dir = ([], [])
+    self.current = symbol
+    self.state = state
+  def __repr__(self):
+    return "%s %s %s %s" % (`self.dir[0]`, `self.state`, `self.current`, `self.dir[1]`)
+  def applies(self, (state_in, symbol_in), (symbol_out, dir_out, state_out)):
+    """Tests whether this transition could have been applied to get to this configuration."""
+    return len(self.dir[not dir_out]) == 0 or \
+           self.dir[not dir_out][0] == symbol_out
+  def apply_trans(self, (state_in, symbol_in), (symbol_out, dir_out, state_out)):
+    """Return a new configuration with transition applied backwards."""
+    new_config = copy.deepcopy(self)
+    new_config.dir[dir_out].insert(0, new_config.current)
+    if len(new_config.dir[not dir_out]) != 0:
+      del new_config.dir[not dir_out][0]
+    new_config.current = symbol_in
+    new_config.state = state_in
+    return new_config
+
+def backtracker((halt_state, halt_symbol), to_state, to_symbol, steps, limit):
+  pos_configs = [Partial_Config(halt_state, halt_symbol)]
+  for i in range(steps):
+    #print " ", len(pos_configs)
+    prev_configs = []
+    for config in pos_configs:
+      for addr, cell in to_state[config.state]:
+        if config.applies(addr, cell):
+          prev_configs.append(config.apply_trans(addr, cell))
+    pos_configs = prev_configs
+    if len(pos_configs) == 0:
+      #print "End", len(pos_configs)
+      return i+1
+    elif len(pos_configs) > limit:
+      break
+  #print "End", len(pos_configs)
+  return False
+
+def test(TTable, steps, limit):
+  # Get initial stat info
+  halts, to_state, to_symbol = get_stats(TTable)
+  max_steps = -1
+  # See if all halts cannot be reached
+  for halt_state, halt_symbol in halts:
+    # Initial Criterion: no halt_state transition goes to the halt_state
+    # For efficiency esp. in multi-symbol situations
+    for cell in TTable[halt_state]:
+      if cell[2] == halt_state:
+        return False
+    this_steps = backtracker((halt_state, halt_symbol), to_state, to_symbol, steps, limit)
+    # If any of the backtracks fail, the whole thing fails.
+    if not this_steps:
+      return False
+    max_steps = max(max_steps, this_steps)
+  # If all halt states cannot be reached:
+  return INFINITE, BACKTRACK, max_steps, "Backtrack"
+      
+def apply_results(results, old_line, log_number):
+  old_results = next[5]
+  return next[0:5]+(results, next[6], log_number, old_results)
+
+if __name__ == "__main__":
+  import sys
+  from Option_Parser import Filter_Option_Parser
+  # Get command line options.
+  opts, args = Filter_Option_Parser(sys.argv, [("backsteps" , int, None, True, True),
+                                               ("limit",     int, 1000, False, True)])
+
+  log_number = opts["log_number"]
+  io = IO.IO(opts["infile"], opts["outfile"], log_number)
+  next = io.read_result()
+  while next:
+    TTable = next[6]
+    # Run the simulator/filter on this machine
+    results = test(TTable, opts["backsteps"], opts["limit"])
+    # Deal with result
+    if results:
+      next = apply_results(results, next, log_number)
+    io.write_result_raw(*next)
+    next = io.read_result()
