a212a4b707ca331cde1c1d5c2d4c9f9198ed5a63
sligocki <sligocki@localhost>
2006-08-22 16:38:30 -0400

    New and improved.  Now without convinience class.
    The speedup is pretty small.  But since it seems to actually be a
    speedup I'm leaving it.

--- Macro/Chain_Tape.py
+++ Macro/Chain_Tape.py
@@ -14,15 +14,6 @@ def reverse(in_list):
   reversed_in_list.reverse()
   return reversed_in_list
 
-class Stack(list):
-  """Generic Stack class based on built-in list."""
-  def pop(self):
-    return list.pop(self, 0)
-  def push(self, item):
-    list.insert(self, 0, item)
-  def copy(self):
-    return Stack([x.copy() for x in self])
-
 class Repeated_Symbol:
   """Slice of tape with repatitions."""
   def __init__(self, symbol, number_of_repetitions):
@@ -40,9 +31,9 @@ class Chain_Tape:
   """Stores the turing machine tape with repetition compression."""
   def init(self, init_symbol, init_dir):
     self.dir = init_dir
-    self.tape = [Stack(), Stack()]
-    self.tape[0].push(Repeated_Symbol(init_symbol, INF))
-    self.tape[1].push(Repeated_Symbol(init_symbol, INF))
+    self.tape = [[], []]
+    self.tape[0].insert(0, Repeated_Symbol(init_symbol, INF))
+    self.tape[1].insert(0, Repeated_Symbol(init_symbol, INF))
     # Measures head displacement from initial position
     self.displace = 0
   def __repr__(self):
@@ -53,8 +44,8 @@ class Chain_Tape:
     new = Chain_Tape()
     new.dir = self.dir
     new.displace = self.displace
-    s0 = self.tape[0].copy()
-    s1 = self.tape[1].copy()
+    s0 = [x.copy() for x in self.tape[0]]
+    s1 = [x.copy() for x in self.tape[1]]
     new.tape = [s0, s1]
     return new
   def get_nonzeros(self, eval_symbol, state_value):
@@ -80,7 +71,7 @@ class Chain_Tape:
       top.num -= 1
       # If there are none left, remove from stack.
       if top.num == 0:
-        stack.pop()
+        stack.pop(0)
     ## Push new symbol
     stack = self.tape[not dir]
     top = stack[0]
@@ -90,7 +81,7 @@ class Chain_Tape:
         top.num += 1
     # Otherwise, just add it seperately.
     else:
-      stack.push(Repeated_Symbol(new_symbol, 1))
+      stack.insert(0, Repeated_Symbol(new_symbol, 1))
     # Update direction
     self.dir = dir
     # Update head displacement
@@ -105,7 +96,7 @@ class Chain_Tape:
     num = self.tape[self.dir][0].num
     if num is INF:
       return INF
-    self.tape[self.dir].pop()
+    self.tape[self.dir].pop(0)
     # Push on new one behind us
     stack = self.tape[not self.dir]
     top = stack[0]
@@ -113,7 +104,7 @@ class Chain_Tape:
       if top.num is not INF:
         top.num += num
     else:
-      stack.push(Repeated_Symbol(new_symbol, num))
+      stack.insert(0, Repeated_Symbol(new_symbol, num))
     # Update head displacement
     if dir:
       self.displace += num
