fe65455e353b0a449d8b64543f30148684fddb04
Shawn Ligocki <sligocki@gmail.com>
2006-10-25 21:02:42 -0400

    Quick_Sim now has more options for proof behavior
    Simple_CTL prints a usage message if the syntax is wrong

--- Quick_Sim.py
+++ Quick_Sim.py
@@ -4,29 +4,24 @@ import sys
 from Macro import Turing_Machine, Chain_Simulator, Block_Finder
 import IO
 
-def run(TTable, block_size=None, back=True):
+def run(TTable, block_size=None, back=True, prover=True, rec=False):
   # Construct Machine (Backsymbol-k-Block-Macro-Machine)
-  m1 = Turing_Machine.Simple_Machine(TTable)
+  m = Turing_Machine.Simple_Machine(TTable)
   # If no explicit block-size given, use inteligent software to find one
   if not block_size:
     Block_Finder.DEBUG = True
-    block_size = Block_Finder.block_finder(m1)
+    block_size = Block_Finder.block_finder(m)
   # Do not create a 1-Block Macro-Machine (just use base machine)
-  if block_size == 1:
-    m2 = m1
-  else:
-    m2 = Turing_Machine.Block_Macro_Machine(m1, block_size)
-  m3 = Turing_Machine.Backsymbol_Macro_Machine(m2)
-  #m4 = Turing_Machine.Block_Macro_Machine(m3, block_size)
-  #m5 = Turing_Machine.Backsymbol_Macro_Machine(m4)
+  if block_size != 1:
+    m = Turing_Machine.Block_Macro_Machine(m, block_size)
+  if back:
+    m = Turing_Machine.Backsymbol_Macro_Machine(m)
 
   global sim
   sim = Chain_Simulator.Simulator()
-  if back:
-    sim.init(m3)
-  else:
-    sim.init(m2)
-  #sim.proof = None
+  sim.init(m, rec)
+  if not prover:
+    sim.proof = None
   extent = 1
   try:
     while sim.op_state == Turing_Machine.RUNNING:
@@ -63,6 +58,18 @@ if "-b" in sys.argv:
 else:
   back = True
 
+if "-p" in sys.argv:
+  sys.argv.remove("-p")
+  prover = False
+else:
+  prover = True
+
+if "-r" in sys.argv:
+  sys.argv.remove("-r")
+  recursive = True
+else:
+  recursive = False
+
 if len(sys.argv) >= 3:
   line = int(sys.argv[2])
 else:
@@ -75,4 +82,4 @@ if len(sys.argv) >= 4:
 else:
   block_size = None
 
-run(ttable, block_size, back)
+run(ttable, block_size, back, prover, recursive)
