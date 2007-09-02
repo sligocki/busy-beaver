56c8c19915e42b9ba238477d88cd5a01033fd5a4
sligocki <sligocki@localhost>
2007-09-02 03:06:07 -0400

    Added Memoization to Macro_sim.
    Parallel Tempering now minimizes.

--- PT.py
+++ PT.py
@@ -1,57 +1,72 @@
 from __future__ import division
-import random, math
+import random, math, sys
 from copy import deepcopy
 
-def PT(B, neigh, f, C, steps):
+def PT(B, neigh, E, C, steps, verbose=False):
   """Runs parallel tempering with temperatures [1/b for b in B]
-     to maximize f where neigh(s) returns a random neighbor to state s
+     to minimize E where neigh(s) returns a random neighbor to state s
      given initial configs C"""
-  #swaps = 0
+  swaps = 0
   assert len(B) == len(C)
   n = len(C)
   for i in range(steps):
+    if verbose and (i % 1000) == 0:
+      print "Steps", i
+      print "Swaps", swaps
+      for c in C:
+        print "%.1f" % E(c),
+      print
+      print
+      sys.stdout.flush()
     # Parallel Phase
     for k in range(n):
-      state = neigh(C[k])
-      if random.random() < math.exp( B[k]*(f(state) - f(C[k])) ):
-        C[k] = state
+      new_state = neigh(C[k], 1/B[k])
+      if random.random() < math.exp( B[k]*(E(C[k]) - E(new_state)) ):
+        C[k] = new_state
     # Swapping Phase
     k = random.randrange(n-1)
-    if random.random() < math.exp( (B[k] - B[k+1])*(f(C[k+1]) - f(C[k]))):
-      #swaps += 1
+    if random.random() < math.exp( (B[k] - B[k+1])*(E(C[k]) - E(C[k+1]))):
+      swaps += 1
       C[k], C[k+1] = C[k+1], C[k]
 
-  #print swaps, steps, swaps/steps
+  if verbose:
+    print "Steps", steps
+    print "Swaps", swaps
+    print "Swaps / Step", swaps / steps
+    for c in C:
+      print "%f" % E(c),
+    print
+    print
   return C
 
 if __name__ == "__main__":
   ## Example: Hard Sphere Model
-  Bi = 5.; Bf = 0.; n = 12
+  Bi = 5.; Bf = 0.0001; n = 12
   B = [Bi + (Bf - Bi)*k/(n-1) for k in range(n)]
 
   X = Y = 20
   C = [ ([ [False]*Y for x in range(X)], 0) for k in range(n)]
 
-  f = lambda s: s[1]
+  E = lambda s: -s[1]
 
-  def neigh((c, E)):
+  def neigh((c, n), T):
     x = random.randrange(X)
     y = random.randrange(Y)
     # If there are neighbors, we can't do anything, stay put.
     if ( (x > 0 and c[x-1][y]) or (x < X-1 and c[x+1][y]) or
          (y > 0 and c[x][y-1]) or (y < Y-1 and c[x][y+1]) ):
-      return c, E
+      return c, n
     # Otherwise toggle this location
     else:
-      E -= (1 if c[x][y] else -1)
+      n -= (1 if c[x][y] else -1) # Number of spheres
       c = deepcopy(c)
       c[x][y] = not c[x][y]
-      return c, E
+      return c, n
 
-  res = PT(B, neigh, f, C, 10000)
+  res = PT(B, neigh, E, C, 10000, True)
   print [x[1] for x in res]
   print
-  for k in range(1):
+  for k in range(2):
     for x in range(X):
       for y in range(Y):
         print ('X' if C[k][0][x][y] else ' '),
