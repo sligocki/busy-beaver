911ca2b6c7721f2d398b711346df83343187bff8
sligocki <sligocki@localhost>
2009-09-01 23:02:10 -0400

    Added optparse! moved wrapping of Stay machines to Macro/Turing_Machine

--- Quick_Sim.py
+++ Quick_Sim.py
@@ -4,16 +4,13 @@ import sys
 from Macro import Turing_Machine, Chain_Simulator, Block_Finder
 import IO
 
-def run(TTable, block_size=None, back=True, prover=True, rec=False, verbose=True):
+def run(TTable, block_size, block_finder_limit, back, prover, rec, verbose):
   # Construct Machine (Backsymbol-k-Block-Macro-Machine)
-  m = Turing_Machine.Simple_Machine(TTable)
-  # If there are any stay instructions, encapsulate this machine
-  if [[dir == Turing_Machine.STAY for state, dir, symbol in row] for row in TTable]:
-    m = Turing_Machine.Block_Macro_Machine(m, 1)
+  m = Turing_Machine.make_machine(TTable)
   # If no explicit block-size given, use inteligent software to find one
   if not block_size:
     Block_Finder.DEBUG = True
-    block_size = Block_Finder.block_finder(m)
+    block_size = Block_Finder.block_finder(m, block_finder_limit)
   # Do not create a 1-Block Macro-Machine (just use base machine)
   if block_size != 1:
     m = Turing_Machine.Block_Macro_Machine(m, block_size)
@@ -52,61 +49,62 @@ def run(TTable, block_size=None, back=True, prover=True, rec=False, verbose=True
     print "Steps:   ", sim.step_num
     print "Nonzeros:", sim.get_nonzeros()
 
-# Main script
-# Backsymbol (default on)
-if "-b" in sys.argv:
-  sys.argv.remove("-b")
-  back = False
-else:
-  back = True
 
-# Prover (default on)
-if "-p" in sys.argv:
-  sys.argv.remove("-p")
-  prover = False
-else:
-  prover = True
-
-# Recursive Prover (default off)
-if "-r" in sys.argv:
-  sys.argv.remove("-r")
-  recursive = True
-else:
-  recursive = False
-
-# Verbose Prover (default off)
-if "-vp" in sys.argv:
-  sys.argv.remove("-vp")
-  Chain_Simulator.Chain_Proof_System.DEBUG = True
-else:
-  Chain_Simulator.Chain_Proof_System.DEBUG = False
-
-# Verbose Simulator (default off)
-if "-vs" in sys.argv:
-  sys.argv.remove("-vs")
-  Chain_Simulator.DEBUG = True
-  verbose = False
-else:
-  Chain_Simulator.DEBUG = False
-  verbose = True
-
-# Brief Simulator output (default off)
-if "-br" in sys.argv:
-  sys.argv.remove("-br")
-  Chain_Simulator.DEBUG = False
-  verbose = False
-
-if len(sys.argv) >= 3:
-  line = int(sys.argv[2])
-else:
-  line = 1
-
-ttable = IO.load_TTable_filename(sys.argv[1], line)
-
-if len(sys.argv) >= 4:
-  block_size = int(sys.argv[3])
-else:
-  block_size = None
-
-run(ttable, block_size, back, prover, recursive, verbose)
+if __name__ == "__main__":
+  from optparse import OptionParser
+ # Parse command line options.
+  usage = "usage: %prog [options] machine_file [line_number]"
+  parser = OptionParser(usage=usage)
+  #parser.set_defaults(verbose=True)
+  parser.add_option("-q", "--quiet", action="store_true", help="Brief output")
+  parser.add_option("--verbose-prover", action="store_true", help="Provide debuggin output from prover")
+  parser.add_option("--verbose-simulator", action="store_true", help="Provide debuggin output from simulator")
+  
+  parser.add_option("-b", "--no-backsymbol", action="store_false", dest="backsymbol", default=True, 
+                    help="Turn off backsymbol macro machine")
+  parser.add_option("-p", "--no-prover", action="store_false", dest="prover", default=True, 
+                    help="Turn off proof system")
+  parser.add_option("-r", "--recursive", action="store_true", default=False, 
+                    help="Turn ON recursive proof system")
+  
+  parser.add_option("-n", "--block-size", type=int, help="Block size to use in macro machine simulator (default is to guess with the block_finder algorithm)")
+  parser.add_option("-l", "--block-finder-limit", type=int, default=200, help="Number of steps to run the block_finder algorithm for (if manual block size not specified) (Default = %default).")
+  (options, args) = parser.parse_args()
+  
+  # Verbose Prover
+  if options.verbose_prover:
+    Chain_Simulator.Chain_Proof_System.DEBUG = True
+  else:
+    Chain_Simulator.Chain_Proof_System.DEBUG = False
+  
+  # Verbose Simulator
+  if options.verbose_simulator:
+    Chain_Simulator.DEBUG = True
+    verbose = False
+  else:
+    Chain_Simulator.DEBUG = False
+    verbose = True
+  
+  # Brief Simulator output
+  if options.quiet:
+    Chain_Simulator.DEBUG = False
+    verbose = False
+  
+  if len(args) < 1:
+    parser.error("Must have at least one argument, machine_file")
+  filename = args[0]
+  
+  if len(args) >= 2:
+    try:
+      line = int(args[1])
+    except ValueError:
+      parser.error("line_number must be an integer.")
+    if line < 1:
+      parser.error("line_number must be >= 1")
+  else:
+    line = 1
+  
+  ttable = IO.load_TTable_filename(filename, line)
+  
+  run(ttable, options.block_size, options.block_finder_limit, options.backsymbol, options.prover, options.recursive, verbose)
 
