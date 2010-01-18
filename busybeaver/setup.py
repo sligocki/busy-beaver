269240661122fbfef9d9fa2744d3adf8d3fe476f
Terry Ligocki <tjligocki@lbl.gov>
2010-01-18 01:24:53 -0800

    Changed weightings on "run_time" computation slightly...

--- busybeaver/setup.py
+++ busybeaver/setup.py
@@ -2,41 +2,41 @@
 
 from distutils.core import setup, Extension
 
-module1 = Extension('Turing_Machine_Sim',
-                    sources=['Turing_Machine_Sim.c','Turing_Machine.c'],
-                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+#module1 = Extension('Turing_Machine_Sim',
+#                    sources=['Turing_Machine_Sim.c','Turing_Machine.c'],
+#                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+#
+#setup(name='Turing_Machine_Sim',
+#      version='1.0',
+#      description='C speedups to Turing machine simulation',
+#      ext_modules=[module1])
 
-setup(name='Turing_Machine_Sim',
-      version='1.0',
-      description='C speedups to Turing machine simulation',
-      ext_modules=[module1])
+#module2 = Extension('Turing_Machine_Count',
+#                    sources=['Turing_Machine_Count.c','Turing_Machine.c'],
+#                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+#
+#setup(name='Turing_Machine_Count',
+#      version='1.0',
+#      description='Turing machine simulation to get tape size as a function of steps',
+#      ext_modules=[module2])
 
-module2 = Extension('Turing_Machine_Count',
-                    sources=['Turing_Machine_Count.c','Turing_Machine.c'],
-                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+#module3 = Extension('Dual_Machine',
+#                    sources=['Dual_Machine.c','Turing_Machine.c'],
+#                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+#
+#setup(name='Dual_Machine',
+#      version='1.0',
+#      description='Run one TM at two different speeds',
+#      ext_modules=[module3])
 
-setup(name='Turing_Machine_Count',
-      version='1.0',
-      description='Turing machine simulation to get tape size as a function of steps',
-      ext_modules=[module2])
-
-module3 = Extension('Dual_Machine',
-                    sources=['Dual_Machine.c','Turing_Machine.c'],
-                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
-
-setup(name='Dual_Machine',
-      version='1.0',
-      description='Run one TM at two different speeds',
-      ext_modules=[module3])
-
-module4 = Extension('Macro_Machine',
-                    sources=['Macro_Machine.c','Turing_Machine.c'],
-                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
-
-setup(name='Macro_Machine',
-      version='1.0',
-      description='Run one TM using macro steps',
-      ext_modules=[module4])
+#module4 = Extension('Macro_Machine',
+#                    sources=['Macro_Machine.c','Turing_Machine.c'],
+#                    extra_compile_args=['-pedantic-errors','-Wno-long-long'])
+#
+#setup(name='Macro_Machine',
+#      version='1.0',
+#      description='Run one TM using macro steps',
+#      ext_modules=[module4])
 
 #module5 = Extension('Tree_Identify',
 #                    sources=['Tree_Identify.c','Turing_Machine.c'],
