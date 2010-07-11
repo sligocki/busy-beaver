41c4963215b78da266c78ac98806fe5cc3d2b511
Shawn Ligocki <sligocki@gmail.com>
2010-07-11 10:27:28 -0400

    Comments and bring back loops through prover. Instead maybe we need a tapes-copied stat.

--- busybeaver/Macro/Chain_Simulator.py
+++ busybeaver/Macro/Chain_Simulator.py
@@ -157,6 +157,7 @@ class Simulator(object):
     print template("Chain:", self.steps_from_chain, self.num_chain_moves)
     if self.prover:
       print template("Rule:", self.steps_from_rule, self.num_rule_moves)
+      print "Loops through prover:", self.prover.num_loops
       print "Rules proven:", len(self.prover.rules)
       if self.prover.recursive:
         print "Recursive rules proven:", self.prover.num_recursive_rules
