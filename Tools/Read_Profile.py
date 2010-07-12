530c3a093b9ca0277c3d3e543fc89811de744dd1
Shawn Ligocki <sligocki@gmail.com>
2010-07-12 02:18:05 -0400

    Beef up Read_Profile.

--- Tools/Read_Profile.py
+++ Tools/Read_Profile.py
@@ -6,15 +6,46 @@ Reads profiling information produced by:
 and prints a few useful bits of info.
 """
 
+from optparse import OptionParser
 import pstats
-import sys
 
-if len(sys.argv) != 2:
-  print >>sys.stderr, "usage: Read_Profile.py filename"
-  sys.exit(1)
+parser = OptionParser(usage="usage: %prog [options] filename [print-params ...]")
+# What to sort by:
+parser.add_option("--time", "--tottime",
+                  action="store_const", const="time", dest="sort_by",
+                  help="Sort by total time in function itself (not it's children). [Default]")
+parser.add_option("--cumulative", "--cumtime",
+                  action="store_const", const="cumulative", dest="sort_by",
+                  help="Sort by cumulative time in function (including in it's children).")
+parser.add_option("--calls", "--ncalls",
+                  action="store_const", const="calls", dest="sort_by",
+                  help="Sort by number of calls to function.")
+# What to print by:
+parser.add_option("--callers",
+                  action="store_const", const="print_callers", dest="print_method",
+                  help="Print callers of functions.")
+parser.add_option("--callees",
+                  action="store_const", const="print_callees", dest="print_method",
+                  help="Print callees of functions.")
+parser.set_defaults(sort_by="time", print_method="print_stats")
+(options, args) = parser.parse_args()
 
-prof_file = sys.argv[1]
+if len(args) < 1:
+  parser.error("Must have at least one argument, filename")
+prof_file = args[0]
+
+def smart_eval(arg):
+  """If arg is text of an int, convert it to an int, otherwise, leave it a string."""
+  try:
+    return int(arg)
+  except ValueError:
+    return arg
+
+print_args = map(smart_eval, args[1:])
+if len(print_args) == 0:
+  print_args = [20]
 
 p = pstats.Stats(prof_file)
-p.strip_dirs().sort_stats("time").print_stats(10)
-p.strip_dirs().sort_stats("cumulative").print_stats(10)
+getattr(p.strip_dirs().sort_stats(options.sort_by), options.print_method)(*print_args)
+# Ex: by default:
+# p.strip_dirs().sort_stats("time").print_stats(20)
