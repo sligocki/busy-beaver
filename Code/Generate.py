2b390e6dc317a234c339297db78852dd8ffee058
Shawn Ligocki <sligocki@gmail.com>
2011-01-08 01:11:48 -0500

    Fix from IO import IO

--- Code/Generate.py
+++ Code/Generate.py
@@ -9,7 +9,7 @@
 import copy
 
 from Turing_Machine import Turing_Machine, Turing_Machine_Runtime_Error
-from IO import IO_old as IO
+from IO_old import IO
 
 # global machine number
 g_machine_num = 0
