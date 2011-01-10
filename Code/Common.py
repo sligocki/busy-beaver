0cea17c046beed4e7a3441f0791feacdb6951b67
Shawn Ligocki <sligocki@gmail.com>
2011-01-09 22:53:21 -0500

    Add Exit_Condition.name

--- Code/Common.py
+++ Code/Common.py
@@ -12,5 +12,24 @@ class Exit_Condition(object):
   INFINITE = 4
   TIME_OUT = 5
   UNKNOWN = (OVER_TAPE, MAX_STEPS, TIME_OUT)
+  
+  @classmethod
+  def name(cls, cond):
+    if cond == Exit_Condition.ERROR:
+      return "Error"
+    elif cond == Exit_Condition.HALT:
+      return "Halt"
+    elif cond == Exit_Condition.OVER_TAPE:
+      return "Over_Tape"
+    elif cond == Exit_Condition.MAX_STEPS:
+      return "Max_Steps"
+    elif cond == Exit_Condition.UNDEF_CELL:
+      return "Undefined_Cell"
+    elif cond == Exit_Condition.INFINITE:
+      return "Infinite"
+    elif cond == Exit_Condition.TIME_OUT:
+      return "Time_Out"
+    else:
+      raise Exception, "Invalid exit condition %r" % cond
 
 HALT_TRANS = (1, 1, -1)
