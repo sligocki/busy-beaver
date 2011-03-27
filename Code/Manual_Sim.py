cfa9a97c42ddbb4809874cb98c479e53e0b14b27
Terry Ligocki <tjligocki@lbl.gov>
2011-03-27 14:49:10 -0700

    First light - you can start the simulator, take a step at a time, get help, and quit/exit/EOF...

--- Code/Manual_Sim.py
+++ Code/Manual_Sim.py
@@ -65,65 +65,145 @@ def print_machine(machine):
   sys.stdout.flush()
 
 
-def run(TTable, block_size, back, prover, recursive, options):
-  # Construct Machine (Backsymbol-k-Block-Macro-Machine)
-  m = Turing_Machine.make_machine(TTable)
+import os
+import cmd
+import rlcompleter
+import readline
 
-  # If no explicit block-size given, use inteligent software to find one
-  if not block_size:
-    block_size = 1
+readline.parse_and_bind ("bind ^I rl_complete")
 
-  if not options.quiet:
-    print_machine(m)
+class BBConsole(cmd.Cmd):
 
-  # Do not create a 1-Block Macro-Machine (just use base machine)
-  if block_size != 1:
-    m = Turing_Machine.Block_Macro_Machine(m, block_size)
-  if back:
-    m = Turing_Machine.Backsymbol_Macro_Machine(m)
+    def __init__(self, TTable, block_size, back, prover, recursive, options):
+        cmd.Cmd.__init__(self)
+        self.cmdnum = 1
+        self.prompt = "%d> " % (self.cmdnum,)
+        self.intro  = "Welcome to the machine!\n"  ## defaults to None
 
-  global sim
-  sim = Simulator.Simulator(m, recursive, enable_prover=prover, init_tape=True,
-                            compute_steps=options.compute_steps,
-                            verbose_simulator=options.verbose_simulator,
-                            verbose_prover=options.verbose_prover,
-                            verbose_prefix="")
-  if options.manual:
-    return  # Let's us run the machine manually. Must be run as python -i Quick_Sim.py
-  try:
-    if options.quiet or options.verbose:  # Note verbose prints inside sim.step()
-      while sim.op_state == Turing_Machine.RUNNING:
-        sim.step()
-    else:
-      # TODO: maybe print based on time
-      while sim.op_state == Turing_Machine.RUNNING:
-        sim.print_self()
-        sim.loop_run(options.print_loops)
-  finally:
-    sim.print_self()
+        # Construct Machine (Backsymbol-k-Block-Macro-Machine)
+        m = Turing_Machine.make_machine(TTable)
 
-  if sim.op_state == Turing_Machine.HALT:
-    print
-    print "Turing Machine Halted!"
-    print
-    if options.compute_steps:
-      print "Steps:   ", sim.step_num
-    print "Nonzeros:", sim.get_nonzeros()
-    print
-  elif sim.op_state == Turing_Machine.INF_REPEAT:
-    print
-    print "Turing Machine proven Infinite!"
-    print "Reason:", sim.inf_reason
-  elif sim.op_state == Turing_Machine.UNDEFINED:
-    print
-    print "Turing Machine reached Undefined transition!"
-    print "State: ", sim.op_details[0][1]
-    print "Symbol:", sim.op_details[0][0]
-    print
-    if options.compute_steps:
-      print "Steps:   ", sim.step_num
-    print "Nonzeros:", sim.get_nonzeros()
-    print
+        # If no explicit block-size given set it to 1
+        if not block_size:
+          block_size = 1
+
+        if not options.quiet:
+          print_machine(m)
+
+        # Do not create a 1-Block Macro-Machine (just use base machine)
+        if block_size != 1:
+          m = Turing_Machine.Block_Macro_Machine(m, block_size)
+        if back:
+          m = Turing_Machine.Backsymbol_Macro_Machine(m)
+
+        self.sim = Simulator.Simulator(m, recursive, enable_prover=prover, init_tape=True,
+                                       compute_steps=options.compute_steps,
+                                       verbose_simulator=options.verbose_simulator,
+                                       verbose_prover=options.verbose_prover,
+                                       verbose_prefix="")
+
+    ## Command definitions ##
+    def do_hist(self, args):
+        """Print a list of commands that have been entered"""
+        num = 1
+        for com in self._hist:
+          print "%4d  %s" % (num,com)
+          num += 1
+
+    def do_step(self,args):
+        """Take on step of the current machine"""
+        self.sim.step()
+
+        if self.sim.op_state == Turing_Machine.HALT:
+          print
+          print "Turing Machine Halted!"
+          print
+          if options.compute_steps:
+            print "Steps:   ", self.sim.step_num
+          print "Nonzeros:", self.sim.get_nonzeros()
+          print
+        elif self.sim.op_state == Turing_Machine.INF_REPEAT:
+          print
+          print "Turing Machine proven Infinite!"
+          print "Reason:", self.sim.inf_reason
+        elif self.sim.op_state == Turing_Machine.UNDEFINED:
+          print
+          print "Turing Machine reached Undefined transition!"
+          print "State: ", self.sim.op_details[0][1]
+          print "Symbol:", self.sim.op_details[0][0]
+          print
+          if options.compute_steps:
+            print "Steps:   ", self.sim.step_num
+          print "Nonzeros:", self.sim.get_nonzeros()
+          print
+
+    def do_exit(self, args):
+        """Exits from the console"""
+        print
+        return -1
+
+    def do_quit(self, args):
+        """Exits from the console"""
+        return self.do_exit(args)
+
+    ## Command definitions to support Cmd object functionality ##
+    def do_EOF(self, args):
+        """Exit on system end of file character"""
+        print
+        return self.do_exit(args)
+
+    def do_help(self, args):
+        """Get help on commands
+           'help' or '?' with no arguments prints a list of commands for which help is available
+           'help <command>' or '? <command>' gives help on <command>
+        """
+        ## The only reason to define this method is for the help text in the doc string
+        cmd.Cmd.do_help(self, args)
+
+    ## Override methods in Cmd object ##
+    def preloop(self):
+        """Initialization before prompting user for commands.
+           Despite the claims in the Cmd documentaion, Cmd.preloop() is not a stub.
+        """
+        cmd.Cmd.preloop(self)   ## sets up command completion
+        self._hist    = []      ## No history yet
+        self._locals  = {}      ## Initialize execution namespace for user
+        self._globals = {}
+
+    def postloop(self):
+        """Take care of any unfinished business.
+           Despite the claims in the Cmd documentaion, Cmd.postloop() is not a stub.
+        """
+        cmd.Cmd.postloop(self)   ## Clean up command completion
+        print "Powering off..."
+
+    def precmd(self, line):
+        """ This method is called after the line has been input but before
+            it has been interpreted. If you want to modifdy the input line
+            before execution (for example, variable substitution) do it here.
+        """
+        self._hist += [ line.strip() ]
+        return line
+
+    def postcmd(self, stop, line):
+        """If you want to stop the console, return something that evaluates to true.
+           If you want to do some post command processing, do it here.
+        """
+        self.cmdnum += 1
+        self.prompt = "%d> " % (self.cmdnum,)
+        return stop
+
+    def emptyline(self):    
+        """Do nothing on empty input line"""
+        self.onecmd(self.lastcmd)
+
+    def default(self, line):       
+        """Called on an input line when the command prefix is not recognized.
+           In that case we execute the line as Python code.
+        """
+        print "Unknown command:",line
+        self.do_help(None)
+
 
 
 if __name__ == "__main__":
@@ -133,19 +213,6 @@ if __name__ == "__main__":
   parser = OptionParser(usage=usage)
   # TODO: One variable for different levels of verbosity.
   # TODO: Combine optparsers from MacroMachine, Enumerate and here.
-  parser.add_option("-q", "--quiet", action="store_true", help="Brief output")
-  parser.add_option("-v", "--verbose", action="store_true",
-                    help="Print step-by-step informaion from simulator "
-                    "and prover.")
-  parser.add_option("--verbose-prover", action="store_true",
-                    help="Provide debugging output from prover.")
-  parser.add_option("--verbose-simulator", action="store_true",
-                    help="Provide debugging output from simulator.")
-  parser.add_option("--verbose-block-finder", action="store_true",
-                    help="Provide debugging output from block_finder.")
-  parser.add_option("--print-loops", type=int, default=10000, metavar="LOOPS",
-                    help="Print every LOOPS loops [Default %default].")
-  
   parser.add_option("-b", "--no-backsymbol", dest="backsymbol",
                     action="store_false", default=True,
                     help="Turn off backsymbol macro machine")
@@ -154,20 +221,19 @@ if __name__ == "__main__":
                     help="Turn off proof system")
   parser.add_option("-r", "--recursive", action="store_true", default=False, 
                     help="Turn on recursive proof system")
-  parser.add_option("--no-steps", dest="compute_steps",
-                    action="store_false", default=True,
-                    help="Don't keep track of base step count (can be expensive"
-                    " to calculate especially with recursive proofs).")
-  parser.add_option("--manual", action="store_true",
-                    help="Don't run any simulation, just set up simulator "
-                    "and quit. (Run as python -i Quick_Sim.py to interactively "
-                    "run simulation.)")
-  
   parser.add_option("-n", "--block-size", type=int,
                     help="Block size to use in macro machine simulator "
                     "(default is to guess with the block_finder algorithm)")
   
   (options, args) = parser.parse_args()
+
+  options.quiet   = False
+  options.verbose = True
+  options.manual  = True
+
+  options.compute_steps = False
+
+  options.print_loops = 1
   
   if options.verbose:
     options.verbose_simulator = True
@@ -193,6 +259,6 @@ if __name__ == "__main__":
   
   ttable = IO.load_TTable_filename(filename, line)
   
-  run(ttable, options.block_size, options.backsymbol, options.prover, 
-              options.recursive, options)
-
+  BBconsole = BBConsole(ttable, options.block_size, options.backsymbol,
+                                options.prover, options.recursive, options)
+  BBconsole.cmdloop() 
