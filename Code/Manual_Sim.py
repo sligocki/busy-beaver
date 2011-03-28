e9cb3df911ca6b4bbb529213c52e7f19dd818595
Terry Ligocki <tjligocki@gmail.com>
2011-03-28 00:19:00 -0700

    More progress - the tape & state can be entered.  Only the tape is currently propagated to a (new) simulator.  At that point the simulator take steps.
    Lots more to do but this is some progress...

--- Code/Manual_Sim.py
+++ Code/Manual_Sim.py
@@ -2,7 +2,7 @@
 
 import sys, string
 
-from Macro import Turing_Machine, Simulator, Block_Finder
+from Macro import Turing_Machine, Simulator, Block_Finder, Tape
 import IO
 
 # White, Red, Blue, Green, Magenta, Cyan, Brown/Yellow
@@ -74,136 +74,264 @@ readline.parse_and_bind ("bind ^I rl_complete")
 
 class BBConsole(cmd.Cmd):
 
-    def __init__(self, TTable, block_size, back, prover, recursive, options):
-        cmd.Cmd.__init__(self)
-        self.cmdnum = 1
-        self.prompt = "%d> " % (self.cmdnum,)
-        self.intro  = "Welcome to the machine!\n"  ## defaults to None
+  def __init__(self, TTable, options):
+    cmd.Cmd.__init__(self)
+    self.cmdnum = 1
+    self.prompt = "%d> " % (self.cmdnum,)
 
-        # Construct Machine (Backsymbol-k-Block-Macro-Machine)
-        m = Turing_Machine.make_machine(TTable)
+    self.TTable = TTable
 
-        # If no explicit block-size given set it to 1
-        if not block_size:
-          block_size = 1
+    self.sim_options = options
 
-        if not options.quiet:
-          print_machine(m)
+    # Construct Machine (Backsymbol-k-Block-Macro-Machine)
+    m = Turing_Machine.make_machine(self.TTable)
 
-        # Do not create a 1-Block Macro-Machine (just use base machine)
-        if block_size != 1:
-          m = Turing_Machine.Block_Macro_Machine(m, block_size)
-        if back:
-          m = Turing_Machine.Backsymbol_Macro_Machine(m)
+    if not self.sim_options.quiet:
+      print_machine(m)
 
-        self.sim = Simulator.Simulator(m, recursive, enable_prover=prover, init_tape=True,
-                                       compute_steps=options.compute_steps,
-                                       verbose_simulator=options.verbose_simulator,
-                                       verbose_prover=options.verbose_prover,
-                                       verbose_prefix="")
+    # If no explicit block-size given set it to 1
+    if not self.sim_options.block_size:
+      self.sim_options.block_size = 1
 
-    ## Command definitions ##
-    def do_hist(self, args):
-        """Print a list of commands that have been entered"""
-        num = 1
-        for com in self._hist:
-          print "%4d  %s" % (num,com)
-          num += 1
+    # Do not create a 1-Block Macro-Machine (just use base machine)
+    if self.sim_options.block_size != 1:
+      m = Turing_Machine.Block_Macro_Machine(m, self.sim_options.block_size)
+    if self.sim_options.backsymbol:
+      m = Turing_Machine.Backsymbol_Macro_Machine(m)
 
-    def do_step(self,args):
-        """Take on step of the current machine"""
-        self.sim.step()
+    self.sim = Simulator.Simulator(m, self.sim_options.recursive,
+                                      enable_prover=self.sim_options.prover,
+                                      init_tape=True,
+                                      compute_steps=self.sim_options.compute_steps,
+                                      verbose_simulator=self.sim_options.verbose_simulator,
+                                      verbose_prover=self.sim_options.verbose_prover,
+                                      verbose_prefix="")
 
-        if self.sim.op_state == Turing_Machine.HALT:
-          print
-          print "Turing Machine Halted!"
-          print
-          if options.compute_steps:
-            print "Steps:   ", self.sim.step_num
-          print "Nonzeros:", self.sim.get_nonzeros()
-          print
-        elif self.sim.op_state == Turing_Machine.INF_REPEAT:
-          print
-          print "Turing Machine proven Infinite!"
-          print "Reason:", self.sim.inf_reason
-        elif self.sim.op_state == Turing_Machine.UNDEFINED:
-          print
-          print "Turing Machine reached Undefined transition!"
-          print "State: ", self.sim.op_details[0][1]
-          print "Symbol:", self.sim.op_details[0][0]
-          print
-          if options.compute_steps:
-            print "Steps:   ", self.sim.step_num
-          print "Nonzeros:", self.sim.get_nonzeros()
-          print
+    print "Welcome to the machine!\n"
 
-    def do_exit(self, args):
-        """Exits from the console"""
-        print
-        return -1
+    self.sim.verbose_print()
 
-    def do_quit(self, args):
-        """Exits from the console"""
-        return self.do_exit(args)
+  ## Command definitions ##
+  def do_EOF(self, args):
+    """Exit on system end of file character"""
+    print
+    return self.do_exit(args)
 
-    ## Command definitions to support Cmd object functionality ##
-    def do_EOF(self, args):
-        """Exit on system end of file character"""
-        print
-        return self.do_exit(args)
+  def do_exit(self, args):
+    """Exits from the console"""
+    print
+    return -1
 
-    def do_help(self, args):
-        """Get help on commands
-           'help' or '?' with no arguments prints a list of commands for which help is available
-           'help <command>' or '? <command>' gives help on <command>
-        """
-        ## The only reason to define this method is for the help text in the doc string
-        cmd.Cmd.do_help(self, args)
+  def do_help(self, args):
+    """Get help on commands
+       'help' or '?' with no arguments prints a list of commands for which help is available
+       'help <command>' or '? <command>' gives help on <command>
+    """
+    ## The only reason to define this method is for the help text in the doc string
+    cmd.Cmd.do_help(self, args)
 
-    ## Override methods in Cmd object ##
-    def preloop(self):
-        """Initialization before prompting user for commands.
-           Despite the claims in the Cmd documentaion, Cmd.preloop() is not a stub.
-        """
-        cmd.Cmd.preloop(self)   ## sets up command completion
-        self._hist    = []      ## No history yet
-        self._locals  = {}      ## Initialize execution namespace for user
-        self._globals = {}
+  def do_hist(self, args):
+    """Print a list of commands that have been entered"""
+    num = 1
+    com_prev = ""
+    for com in self._hist:
+      if com != "" and com != com_prev:
+        print "%4d  %s" % (num,com)
+        com_prev = com
+      num += 1
 
-    def postloop(self):
-        """Take care of any unfinished business.
-           Despite the claims in the Cmd documentaion, Cmd.postloop() is not a stub.
-        """
-        cmd.Cmd.postloop(self)   ## Clean up command completion
-        print "Powering off..."
+  def do_quit(self, args):
+    """Exits from the console"""
+    return self.do_exit(args)
 
-    def precmd(self, line):
-        """ This method is called after the line has been input but before
-            it has been interpreted. If you want to modifdy the input line
-            before execution (for example, variable substitution) do it here.
-        """
-        self._hist += [ line.strip() ]
-        return line
+  def do_step(self,args):
+    """Take on step of the current machine"""
+    self.sim.step()
 
-    def postcmd(self, stop, line):
-        """If you want to stop the console, return something that evaluates to true.
-           If you want to do some post command processing, do it here.
-        """
-        self.cmdnum += 1
-        self.prompt = "%d> " % (self.cmdnum,)
-        return stop
+    if self.sim.op_state == Turing_Machine.HALT:
+      print
+      print "Turing Machine Halted!"
+      print
+      if options.compute_steps:
+        print "Steps:   ", self.sim.step_num
+      print "Nonzeros:", self.sim.get_nonzeros()
+      print
+    elif self.sim.op_state == Turing_Machine.INF_REPEAT:
+      print
+      print "Turing Machine proven Infinite!"
+      print "Reason:", self.sim.inf_reason
+    elif self.sim.op_state == Turing_Machine.UNDEFINED:
+      print
+      print "Turing Machine reached Undefined transition!"
+      print "State: ", self.sim.op_details[0][1]
+      print "Symbol:", self.sim.op_details[0][0]
+      print
+      if options.compute_steps:
+        print "Steps:   ", self.sim.step_num
+      print "Nonzeros:", self.sim.get_nonzeros()
+      print
 
-    def emptyline(self):    
-        """Do nothing on empty input line"""
-        self.onecmd(self.lastcmd)
+  def do_tape(self, args):
+    self.stdout.write("\n")
+    self.stdout.write("   Tape: ")
+    self.stdout.flush()
 
-    def default(self, line):       
-        """Called on an input line when the command prefix is not recognized.
-           In that case we execute the line as Python code.
-        """
-        print "Unknown command:",line
-        self.do_help(None)
+    tape_state_string = self.stdin.readline()
 
+    tape_state_tokens = tape_state_string.split()
+
+    tape_parse = [tape_state_token.split("^") for tape_state_token in tape_state_tokens]
+
+    tape_length = 0
+
+    state_index = -1
+    back_index = -1
+
+    token_length = len(tape_parse)
+    symbol_length = len(tape_parse[0][0])
+
+    for i in xrange(token_length):
+      token = tape_parse[i]
+      if len(token) == 2:
+        tape_length += 1
+        if len(token[0]) != symbol_length:
+          print "Tape symbol lengths don't match"
+          return
+        if symbol_length == 1:
+          new_symbol = int(token[0])
+        else:
+          new_symbol = Turing_Machine.Block_Symbol([int(c) for c in token[0]])
+        token[0] = new_symbol
+      elif token[0][0] == "(":
+        if len(token[0]) != symbol_length + 2:
+          print "Back symbol length doesn't match tape symbol length"
+          return
+        back_index = i
+      elif token[0][0] == "<":
+        state_index = i
+        tape_dir = 0
+      elif token[0][-1] == ">":
+        state_index = i
+        tape_dir = 1
+      else:
+        print "Unrecognized tape entry: %s" % (token,)
+        return
+
+    if back_index > -1:
+      if tape_dir == 0:
+        if state_index > back_index:
+          print "State and backsymbol are out of order"
+          return
+        elif state_index + 1 != back_index:
+          print "State and backsymbol aren't together"
+          return
+      else:
+        if state_index < back_index:
+          print "State and backsymbol are out of order"
+          return
+        elif state_index - 1 != back_index:
+          print "State and backsymbol aren't together"
+          return
+      new_back_symbol = tape_parse[back_index][0][1:-1]
+    else:
+      new_back_symbol = ""
+    
+    if tape_dir == 0:
+      new_state = tape_parse[state_index][0][1]
+    else:
+      new_state = tape_parse[state_index][0][0]
+        
+    new_tape = Tape.Chain_Tape()
+    new_tape.dir = tape_dir
+
+    new_tape.tape = [[], []]
+    for i in xrange(token_length):
+      if i < state_index and (back_index == -1 or i < back_index):
+        if tape_parse[i][1] == "Inf":
+          new_tape.tape[0].append(Tape.Repeated_Symbol(tape_parse[i][0],Tape.INF))
+        else:
+          new_tape.tape[0].append(Tape.Repeated_Symbol(tape_parse[i][0],int(tape_parse[i][1])))
+      if i > state_index and (back_index == -1 or i > back_index):
+        if tape_parse[i][1] == "Inf":
+          new_tape.tape[1].append(Tape.Repeated_Symbol(tape_parse[i][0],Tape.INF))
+        else:
+          new_tape.tape[1].append(Tape.Repeated_Symbol(tape_parse[i][0],int(tape_parse[i][1])))
+
+    new_tape.tape[1].reverse()
+    new_tape.displace = 0
+
+    self.sim_options.block_size = symbol_length
+
+    if back_index >= 0:
+      self.sim_options.backsymbol = True
+    else:
+      self.sim_options.backsymbol = False
+
+    # Construct Machine (Backsymbol-k-Block-Macro-Machine)
+    m = Turing_Machine.make_machine(self.TTable)
+
+    # If no explicit block-size given set it to 1
+    if not self.sim_options.block_size:
+      self.sim_options.block_size = 1
+
+    # Do not create a 1-Block Macro-Machine (just use base machine)
+    if self.sim_options.block_size != 1:
+      m = Turing_Machine.Block_Macro_Machine(m, self.sim_options.block_size)
+    if self.sim_options.backsymbol:
+      m = Turing_Machine.Backsymbol_Macro_Machine(m)
+
+    self.sim = Simulator.Simulator(m, self.sim_options.recursive,
+                                      enable_prover=self.sim_options.prover,
+                                      init_tape=True,
+                                      compute_steps=self.sim_options.compute_steps,
+                                      verbose_simulator=self.sim_options.verbose_simulator,
+                                      verbose_prover=self.sim_options.verbose_prover,
+                                      verbose_prefix="")
+
+    self.sim.tape = new_tape
+
+    self.stdout.write("\n")
+    self.sim.verbose_print()
+
+  ## Override methods in Cmd object ##
+  def preloop(self):
+    """Initialization before prompting user for commands.
+       Despite the claims in the Cmd documentaion, Cmd.preloop() is not a stub.
+    """
+    cmd.Cmd.preloop(self)   ## sets up command completion
+    self._hist    = []      ## No history yet
+    self._locals  = {}      ## Initialize execution namespace for user
+    self._globals = {}
+
+  def postloop(self):
+    """Take care of any unfinished business.
+       Despite the claims in the Cmd documentaion, Cmd.postloop() is not a stub.
+    """
+    cmd.Cmd.postloop(self)   ## Clean up command completion
+    print "Powering down...\n"
+
+  def precmd(self, line):
+    """ This method is called after the line has been input but before
+        it has been interpreted. If you want to modifdy the input line
+        before execution (for example, variable substitution) do it here.
+    """
+    self._hist += [ line.strip() ]
+    return line
+
+  def postcmd(self, stop, line):
+    """If you want to stop the console, return something that evaluates to true.
+       If you want to do some post command processing, do it here.
+    """
+    self.cmdnum += 1
+    self.prompt = "%d> " % (self.cmdnum,)
+    return stop
+
+  def default(self, line):       
+    """Called on an input line when the command prefix is not recognized.
+       In that case we execute the line as Python code.
+    """
+    print "Unknown command:",line
+    self.do_help(None)
 
 
 if __name__ == "__main__":
@@ -259,6 +387,5 @@ if __name__ == "__main__":
   
   ttable = IO.load_TTable_filename(filename, line)
   
-  BBconsole = BBConsole(ttable, options.block_size, options.backsymbol,
-                                options.prover, options.recursive, options)
+  BBconsole = BBConsole(ttable, options)
   BBconsole.cmdloop() 
