58f235af6cb697218dec4990a56726ef20dc5fb9
Terry Ligocki <tjligocki@lbl.gov>
2011-05-23 12:27:09 -0700

    More work on the recurrence relations - very verbose now, not soup yet (but it seems to be working!)...

--- Code/Recur.py
+++ Code/Recur.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys, string, copy
+import sys, string, copy, numpy
 
 from Macro import Turing_Machine, Simulator, Block_Finder
 import IO
@@ -123,9 +123,13 @@ def run(TTable, block_size, back, prover, recursive, options):
       min_config = strip_config(sim.state,sim.dir,sim.tape.tape)
 
       if min_config in groups:
-        groups[min_config].append([copy.deepcopy(sim.tape.tape),sim.step_num])
+        groups[min_config].append([copy.deepcopy(sim.tape.tape[0][1:]),
+                                   copy.deepcopy(sim.tape.tape[1][1:]),
+                                   sim.step_num])
       else:
-        groups[min_config] = [[copy.deepcopy(sim.tape.tape),sim.step_num],]
+        groups[min_config] = [[copy.deepcopy(sim.tape.tape[0][1:]),
+                               copy.deepcopy(sim.tape.tape[1][1:]),
+                               sim.step_num],]
 
     total_loops += 1;
 
@@ -137,14 +141,95 @@ def run(TTable, block_size, back, prover, recursive, options):
   for min_config in sorted_keys:
     group = groups[min_config]
 
-    if len(group) > 2:
+    if len(group) >= 4:
       print Output_Machine.display_ttable(TTable),"  ",
       print "%5d" % len(group),"  ",
       print "(%5d)" % len(groups),"  ",
       print min_config
 
+      growth = [[] for i in xrange(len(group[0][0]) + len(group[0][1]) + 1)]
+      for config in group:
+        index = 0
+        for symbol in config[0]:
+          growth[index].append(symbol.num)
+          index += 1
+        for symbol in config[1]:
+          growth[index].append(symbol.num)
+          index += 1
+        growth[index].append(config[2])
+
+      recur_all = True
+
+      for series in growth:
+        series = [term for term in series if term < 100000000]
+        if len(series) > 20:
+          series = series[len(series)-20:]
+
+        print "     ",series
+
+        recur_this = False
+
+        for n in xrange(0,len(series)/2):
+          a = [[1,] for i in xrange(len(series)-n)]
+          for m in xrange(0,n):
+            for i in xrange(len(series)-n):
+              a[i].append(series[m+i])
+
+          b = [[series[i],] for i in xrange(n,len(series))]
+
+          A = numpy.array(a)
+          B = numpy.array(b)
+
+          [x,residue,rank,sv] = numpy.linalg.lstsq(A,B)
+          residue = residue[0]
+          print "        ",n,residue,
+
+          if residue < 1e-12:
+            print "successful",
+
+            int_coefs = [int(round(coef[0])) for coef in x]
+            int_coefs.reverse()
+
+            constant = int_coefs[-1]
+            int_coefs = int_coefs[:-1]
+
+            print " F(n) =",
+
+            for i in xrange(len(int_coefs)):
+              if i == 0:
+                if int_coefs[i] == 1:
+                  print "F(n-%d)" % (i+1,),
+                else:
+                  print int_coefs[i],"F(n-%d)" % (i+1,),
+              else:
+                if int_coefs[i] != 0:
+                  if int_coefs[i] > 0:
+                    if int_coefs[i] == 1:
+                      print "+","F(n-%d)" % (i+1,),
+                    else:
+                      print "+",int_coefs[i],"F(n-%d)" % (i+1,),
+                  else:
+                    if int_coefs[i] == -1:
+                      print "-","F(n-%d)" % (i+1,),
+                    else:
+                      print "-",-int_coefs[i],"F(n-%d)" % (i+1,),
+
+            if len(int_coefs) > 0:
+              print "+",
+
+            print constant
+
+            recur_this = True
+            break
+          else:
+            print
+
+        recur_all = recur_all and recur_this
+
       # for elem in group:
       #   print elem
+      print "     ",recur_all
+      print
 
       break
 
