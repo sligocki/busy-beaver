151c20397a7e5328b0b52d015f1a2a52de1644c7
Terry Ligocki <tjligocki@lbl.gov>
2012-01-04 03:59:59 -0800

    Added a routine to test the alarms...

---
+++ Code/Alarm_Test.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+#
+# Alarm_Test.py
+#
+"""
+Test the Python alarms...
+"""
+
+import signal
+
+# SignalPlus allows millisecond alarm, but needs to be compiled seperately.
+# Fall back to inferior signal.alarm if we have to.
+try:
+  from signalPlus import alarm
+except ImportError:
+  import sys; print >>sys.stderr, "Alarm.py: Failed to import signalPlus, falling back to signal."
+  from signal import alarm
+
+class AlarmException(Exception):
+  """An exception to be tied to a timer running out."""
+
+class Alarm(object):
+  """Singleton class that takes care of setting and turning off timer."""
+  def __init__(self):
+    self.is_alarm_on = False
+  
+  def set_alarm(self, time):
+    self.is_alarm_on = True
+    alarm(time)
+  
+  def cancel_alarm(self):
+    self.is_alarm_on = False
+    alarm(0)
+  
+  def alarm_handler(self, signum, frame):
+    if self.is_alarm_on:
+      raise AlarmException, "Timeout!"
+
+ALARM = Alarm()
+
+# Attach the alarm signal to the alarm exception.
+#   so signalPlus.alarm will cause a catchable exception.
+signal.signal(signal.SIGALRM, ALARM.alarm_handler)
+
+try:
+  ALARM.set_alarm(2.5)
+
+  count = 0
+  while True:
+    count += 1
+    if (count % 1000000 == 0):
+      print count
+
+except AlarmException:
+  print "loop interrupted at",count
