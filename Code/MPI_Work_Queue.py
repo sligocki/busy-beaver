bf0a2daee8f124848ccb7bc890999f7ea105654b
Terry Ligocki <tjligocki@gmail.com>
2012-01-19 19:35:15 -0800

    Guarded a "pout.write" with "if pout:"...

--- Code/MPI_Work_Queue.py
+++ Code/MPI_Work_Queue.py
@@ -214,7 +214,8 @@ class Master(object):
   def print_stats(self):
     cur_time = time.time()
     if cur_time - self.last_time >= self.sample_time:
-      self.pout.write("Master queue info: %s\n" % (self.get_stats(),))
-      # self.pout.flush()
+      if self.pout:
+        self.pout.write("Master queue info: %s\n" % (self.get_stats(),))
+        # self.pout.flush()
 
       self.last_time = cur_time
