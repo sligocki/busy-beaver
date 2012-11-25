fd9cdd0084dd8f2fdb18c136200291ef048f4355
Terry Ligocki <tjligocki@lbl.gov>
2012-11-24 22:07:23 -0800

    Changed the wait between queue size updates from 10 to 1 second...

--- Code/MPI_Work_Queue.py
+++ Code/MPI_Work_Queue.py
@@ -46,7 +46,7 @@ class MPI_Worker_Work_Queue(Work_Queue.Work_Queue):
 
     # Time and interval used for reporting queue size.
     self.last_report_time = time.time()
-    self.report_interval = 10
+    self.report_interval = 1
     
     # Where we spend our time.
     self.get_time     = 0.0
@@ -165,7 +165,7 @@ class Master(object):
 
     # Time and interval used for updating worker max queue size.
     self.last_update_time = 0
-    self.update_interval = 10  # Seconds
+    self.update_interval = 1  # Seconds
 
     # Where we spend our time.
     # Downtime waiting for message from workers.
