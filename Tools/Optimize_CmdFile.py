b8a3f4a95e2577bfafe11a3b6ad27f7222ccaddf
Terry Ligocki <tjligocki@gmail.com>
2012-12-16 22:04:37 -0800

    Changes to make our serial and parallel codes/runs play better together.  Also, added a python script to input log files and output a list of commands sorted by TMs identified per second (from highest to lowest)...

---
+++ Tools/Optimize_CmdFile.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+#
+# Optimize_Cmdfile.py
+#
+"""
+Use log files to produce optimized command files by determining TMs identified
+per second for each filter and sorting by that rate.
+"""
+
+def getrate(x):
+  return x[1][2]
+
+import sys,string,operator
+
+filters = {}
+
+unknowns = 0
+
+for line in sys.stdin:
+  pieces = string.split(line,"|")
+  number = string.split(pieces[5])
+
+  commandline = pieces[2]
+  splitline = string.split(commandline)
+
+  command = splitline[0]
+
+  runtime = int(pieces[4])
+  delta_unknowns = int(number[3])
+
+  unknowns += delta_unknowns
+
+  if command not in ['Enumerate.py', 'Assign_Undecideds_Filter', 'mpirun']:
+    if commandline in filters:
+      filters[commandline][0] += -delta_unknowns
+      filters[commandline][1] += runtime
+    else:
+      filters[commandline] = [-delta_unknowns,runtime]
+
+for (filter,stats) in filters.iteritems():
+  delta_unknowns = stats[0]
+  runtime = stats[1]
+
+  if runtime == 0:
+    stats.append(0.0)
+  else:
+    stats.append(float(delta_unknowns)/runtime)
+
+# new_order = sorted(filters, key=filters.get, reverse=True)
+new_order = sorted(filters.iteritems(), key=getrate, reverse=True)
+
+for filter in new_order:
+  print filter[0]
