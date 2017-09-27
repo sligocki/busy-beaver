3bb7d5f2b42b8891fae6a2eb34d70733d35dc33c
Shawn Ligocki <sligocki@gmail.com>
2017-09-27 15:31:09 -0400

    Fix Quick_Sim.py printing

--- Code/Quick_Sim.py
+++ Code/Quick_Sim.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     options.verbose_prover = True
     options.verbose_block_finder = True
 
-  if options.print_loops > options.loops:
+  if options.loops and options.print_loops > options.loops:
     options.print_loops = options.loops
 
   if len(args) < 1:
