04243f08b637577996db14eb1349be3081ec9e48
Shawn Ligocki <sligocki@gmail.com>
2020-09-04 16:22:58 -0400

    Add a python script for randomly shuffling and splitting files up.

---
+++ LazyBeaver/shuffle_split.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Randomly shuffle lines from a file and split them out into N new files.
+"""
+
+import argparse
+import random
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile")
+parser.add_argument("num_chunks", type=int)
+args = parser.parse_args()
+
+# NOTE: This reads all lines into memory and then shuffles them, so is only
+# a good idea for a small-ish file. For larger files, a more clever algorithm
+# would be needed.
+infile = open(args.infile, "r")
+lines = infile.readlines()
+random.shuffle(lines)
+
+lines_per_chunk = len(lines) / args.num_chunks
+chunk_breaks = [round(lines_per_chunk * chunk_num) for chunk_num in range(args.num_chunks + 1)]
+for chunk_num in range(args.num_chunks):
+  outfilename = f"{args.infile}.part_{chunk_num}_of_{args.num_chunks}"
+  outfile = open(outfilename, "w")
+  outfile.writelines(lines[chunk_breaks[chunk_num] : chunk_breaks[chunk_num + 1]])
