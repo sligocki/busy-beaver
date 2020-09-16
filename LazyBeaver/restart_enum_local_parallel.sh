f940a85c223e803b17198622bbf5f66513726110
Terry Ligocki <tjligocki@lbl.gov>
2020-09-16 16:41:02 -0700

    Added the capability to checkpoint and stop a run and then restart the run redistributing the entire stack of remaining TMs.  This process is still not entirely automated and, frankly, a bit clunky BUT it seems to work...

--- LazyBeaver/restart_enum_local_parallel.sh
+++ LazyBeaver/restart_enum_local_parallel.sh
@@ -16,24 +16,16 @@ NUM_PROCESSES=$5
 
 DATA_DIR="./data/${NUM_STATES}x${NUM_SYMBOLS}/${INIT_STEPS}/"
 
-rm -rf $DATA_DIR
-mkdir -p $DATA_DIR
-
 make
 
 echo
 date
-echo "(1) Enumerating a small number of steps to get a bunch of machines."
-./lazy_beaver_enum $NUM_STATES $NUM_SYMBOLS $INIT_STEPS ${DATA_DIR}/steps_init ${DATA_DIR}/tms_init
-
-echo
-date
-echo "(2) Splitting those machines into chunks."
+echo "(1) Splitting those machines into chunks."
 ./shuffle_split.py ${DATA_DIR}/tms_init $NUM_PROCESSES
 
 echo
 date
-echo "(3) Continue enumeration on each chunk as a separate process in parallel."
+echo "(2) Continue enumeration on each chunk as a separate process in parallel."
 for chunk_num in $(seq 0 $((NUM_PROCESSES - 1))); do
   INPUT_TMS=${DATA_DIR}/tms_init.part_${chunk_num}_of_${NUM_PROCESSES}
   ./continue_enum $INPUT_TMS $MAX_STEPS ${DATA_DIR}/steps_part_${chunk_num}_of_${NUM_PROCESSES} ${DATA_DIR}/stack_part_${chunk_num}_of_${NUM_PROCESSES} ${chunk_num} &
