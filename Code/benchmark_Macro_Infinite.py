01d4f7bcf7f66f72bc5c43f93408f0a065fdac53
Shawn Ligocki <sligocki@gmail.com>
2022-02-08 11:41:02 -0500

    Add Benchmark for Macro Machine inf detection. Before the previous change this took 20s for block-size 20.

---
+++ Code/benchmark_Macro_Infinite.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+
+import IO
+from Macro import Turing_Machine
+
+
+# Simple repeating machine.
+ttable = IO.parse_ttable("0RA 1LA")
+tm = Turing_Machine.make_machine(ttable)
+for block_size in range(2, 21):
+  macro_machine = Turing_Machine.Block_Macro_Machine(tm, block_size)
+  macro_symbol = Turing_Machine.Block_Symbol((0, 1) + (0,) * (block_size - 2))
+
+  print "Starting", block_size
+  start_time = time.time()
+  trans = macro_machine.get_trans_object(macro_symbol,
+                                         macro_machine.init_state,
+                                         Turing_Machine.RIGHT)
+  end_time = time.time()
+  print "Finished", block_size, end_time - start_time
+
+  assert trans.condition in (Turing_Machine.INF_REPEAT,
+                             Turing_Machine.GAVE_UP), trans.__dict__
+
+print "Finished"
