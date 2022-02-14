40a9bf93391e25bd21e7c46f0f04d153862069ed
Shawn Ligocki <sligocki@gmail.com>
2022-02-14 14:19:35 -0500

    Basic sbatch scripts for running parallel enumeration via Slurm array jobs.

---
+++ Tools/sbatch_enum_array.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -J bb
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH -a 0-999%100
+#SBATCH -t 4:00:00
+#SBATCH -o enum-%A_%a.txt
+
+set -e
+set -u
+set -x
+
+WORK_DIR=$1
+shift
+
+INDEX=$(printf "%08d" $SLURM_ARRAY_TASK_ID)
+
+if [ -f "${WORK_DIR}/init.split.unknown.${INDEX}" ]; then
+  time python2 Code/Enumerate.py \
+    --allow-no-halt --no-reverse-engineer --no-ctl \
+    --force --time=0 \
+    --infile=${WORK_DIR}/init.split.unknown.${INDEX} \
+    --outfile=${WORK_DIR}/array_${INDEX}.out \
+    "$@"
+
+  echo Success
+
+else
+  echo No machines to enumerate
+fi
---
+++ Tools/sbatch_enum_init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -J bb_init
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH -t 1:00:00
+#SBATCH -o enum_init-%j.txt
+
+set -e
+set -u
+set -x
+
+WORK_DIR=$1
+shift
+
+NUM_CHUNKS=1000
+
+BASENAME=${WORK_DIR}/init
+mkdir -p ${WORK_DIR}
+
+# Enumerate a handful of machines.
+time python2 Code/Enumerate.py \
+  --allow-no-halt --no-reverse-engineer --no-ctl \
+  --breadth-first --num-enum=10000 \
+  --force --time=0 \
+  --outfile=${BASENAME}.out \
+  "$@"
+
+time python2 Tools/Integrate_Data.py ${BASENAME}
+
+time shuf ${BASENAME}.unknown > ${BASENAME}.shuffled.unknown
+
+num_lines=$(cat ${BASENAME}.shuffled.unknown | wc -l)
+num_lines_per_chunk=$((num_lines / NUM_CHUNKS + 1))
+
+time split --lines=${num_lines_per_chunk} --numeric-suffixes --suffix-length=8 \
+  ${BASENAME}.shuffled.unknown \
+  ${BASENAME}.split.unknown.
+
+echo Success
