a99d51b055dc539d33e2c4020b2267de63db952a
Terry Ligocki <tjligocki@gmail.com>
2022-03-10 19:37:42 -0800

    Made the debug flag I added more descriptive...

--- Code/Enumerate.py
+++ Code/Enumerate.py
@@ -131,7 +131,7 @@ class Enumerator(object):
         self.pout.write("Ran out of TMs...\n");
         break
 
-      if self.options.debug:
+      if self.options.debug_print_current:
         print("----- Debug - Current TM:",Output_Machine.display_ttable(tm.get_TTable()))
         sys.stdout.flush()
 
@@ -379,7 +379,7 @@ def main(args):
   enum_parser.add_option("--no-first-1rb", dest="first_1rb",
                          action="store_false", default=True,
                          help="Allow first transition to be anything (not just restricted to A1->1RB).")
-  enum_parser.add_option("-d","--debug", action="store_true", default=False)
+  enum_parser.add_option("--debug-print-current", dest="debug_print_current", action="store_true", default=False)
   parser.add_option_group(enum_parser)
 
   Macro_Simulator.add_option_group(parser)
