cf31aa5369ee24e9cf1f1f2dd3c6f6c7eaf551dd
Shawn Ligocki <sligocki@gmail.com>
2022-04-27 13:34:34 -0400

    Add simple script to count # records in protobuf file (10x faster than TM_Analyze).

---
+++ Code/IO_Count.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+"""Simple tool to count # of TM records in a protobuf file."""
+
+import argparse
+from pathlib import Path
+import time
+
+import IO
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("tm_file", type=Path, nargs="+")
+  parser.add_argument("--print-freq", type=int, default=1_000_000)
+  args = parser.parse_args()
+
+  count = 0
+  start_time = time.time()
+  for filename in args.tm_file:
+    file_count = 0
+    try:
+      with open(filename, "rb") as infile:
+        reader = IO.Proto.Reader(infile)
+        for tm_record in reader:
+          file_count += 1
+          if args.print_freq and (file_count % args.print_freq == 0):
+            print(f" ... {file_count:_} ({time.time() - start_time:_.2f}s)")
+    except IO.Proto.IO_Error:
+      print(f"ERROR: {filename} has unexpected EOF. Moving on.")
+    print(f"# TMs {filename}: {file_count:_} ({time.time() - start_time:_.2f}s)")
+    count += file_count
+
+  print(f"Total # TMs: {count:_} ({time.time() - start_time:_.2f}s)")
+
+if __name__ == "__main__":
+  main()
