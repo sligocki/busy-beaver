8d2314548962d047856b32c1cad2361651ee8637
Shawn Ligocki <sligocki@gmail.com>
2022-06-09 12:31:38 -0400

    Upgrade to new IO

--- Code/TM_Print.py
+++ Code/TM_Print.py
@@ -10,42 +10,39 @@ import IO
 from Macro import Turing_Machine
 
 
-def ttable_to_markdown(ttable):
+def tm_to_markdown(tm : Turing_Machine.Simple_Machine) -> str:
   """
   Write TM transition table to a Markdown table for use in, say, blog posts.
-  
+
   Ex:
     |     |  0  |  1  |  2  |  3  |
     | :-: | :-: | :-: | :-: | :-: |
     |  A  | 1RB | 2LA | 1RA | 1RA |
     |  B  | 1LB | 1LA | 3RB | 1RZ |
   """
-  
-  num_states = len(ttable)
-  num_symbols = len(ttable[0])
 
   result = ""
 
   # Column names
   result += "|     |"
-  for symbol in range(num_symbols):
+  for symbol in range(tm.num_symbols):
     result += "  %d  |" % symbol
   result += "\n"
-  
-  result += "| :-: " * (num_symbols + 1) + "|\n"
+
+  result += "| :-: " * (tm.num_symbols + 1) + "|\n"
 
   # Transitions
-  for state_in in range(num_states):
+  for state_in in range(tm.num_states):
     # Row name
     result += "|  %c  |" % Turing_Machine.states[state_in]
-    for symbol_in in range(num_symbols):
-      (symbol_out, dir_out, state_out) = ttable[state_in][symbol_in]
-      if symbol_out == -1:
+    for symbol_in in range(tm.num_symbols):
+      trans = tm.get_trans_object(state_in = state_in, symbol_in = symbol_in)
+      if trans.condition == Turing_Machine.UNDEFINED:
         result += " --- |"  # Undefined transition
       else:
-        result += " %c%c%c |" % (Turing_Machine.symbols[symbol_out],
-                                 Turing_Machine.dirs[dir_out],
-                                 Turing_Machine.states[state_out])
+        result += " %c%c%c |" % (Turing_Machine.symbols[trans.symbol_out],
+                                 Turing_Machine.dirs[trans.dir_out],
+                                 Turing_Machine.states[trans.state_out])
     result += "\n"
 
   return result
@@ -53,14 +50,14 @@ def ttable_to_markdown(ttable):
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("tm_file", type=Path)
-  parser.add_argument("line_num", type=int, nargs="?", default=1)
+  parser.add_argument("record_num", type=int, nargs="?", default=0)
   parser.add_argument("--format", choices = ["markdown"], default="markdown")  # TODO: text, HTML?
   args = parser.parse_args()
-  
-  ttable = IO.load_TTable_filename(args.tm_file, args.line_num)
+
+  tm = IO.load_tm(args.tm_file, args.record_num)
 
   if args.format == "markdown":
-    print(ttable_to_markdown(ttable))
+    print(tm_to_markdown(tm))
   else:
     raise Exception("Unexpected value for --format: %s" % (args.format,))
 
