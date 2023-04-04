64b5608751661884209af0b39b13ff76c9ddbf71
Shawn Ligocki <sligocki@gmail.com>
2023-04-03 21:42:31 -0400

    1) Switch tape format to support both limited and complete configurations. 2) Swtich rule application logic to return detailed errors.

--- rust/src/config.rs
+++ rust/src/config.rs
@@ -4,21 +4,21 @@ use crate::tm::{Dir, State, Symbol};
 
 pub type Count = i64;
 
-#[derive(Debug)]
-pub enum Rep<RepT> {
-    Finite(RepT),
-    Infinite,
-}
-
 // A block of TM symbols with a repetition count. Ex:
 //      110^13  or  10^{x+4}
 #[derive(Debug)]
 pub struct RepBlock<RepT> {
     pub block: Vec<Symbol>,
-    pub rep: Rep<RepT>,
+    pub rep: RepT,
 }
 
-pub type HalfTape<RepT> = Vec<RepBlock<RepT>>;
+#[derive(Debug)]
+pub struct HalfTape<RepT> {
+    pub data: Vec<RepBlock<RepT>>,
+    // Is this HalfTape complete (implicitly extended by 0^inf) or only a
+    // limited finite portion of the tape?
+    pub is_complete: bool,
+}
 pub type Tape<RepT> = EnumMap<Dir, HalfTape<RepT>>;
 
 #[derive(Debug)]
@@ -27,3 +27,43 @@ pub struct RepConfig<RepT> {
     pub state: State,
     pub dir: Dir,
 }
+
+pub type RepConfigConcrete = RepConfig<Count>;
+
+// impl RepConfigConcrete {
+//     // Read 1 symbol in front of the TM head.
+//     pub fn pop_one_front(&mut self) -> Symbol {
+//         let front = &self.tape[self.dir];
+//         match front.last_mut() {
+//             None => {
+//                 if (front.is_complete) {
+//
+//                 }
+//             }
+//             Some(RepBlock {
+//                 rep: Rep::Infinite, ..
+//             }) => {}
+//             Some(RepBlock {
+//                 rep: Rep::Finite(1), ..
+//             }) => {
+//                 self.tape[self.dir].pop();
+//             }
+//             Some(RepBlock {
+//                 rep: Rep::Finite(rep), ..
+//             }) => {
+//                 *rep -= 1;
+//             }
+//         }
+//     }
+//     // Write 1 symbol behind the TM head.
+//     pub fn push_one_back(&mut self, write: Symbol) {
+//
+//     }
+//
+//     pub fn front_block(&self) -> Vec<Symbol> {
+//         match self.tape[self.dir].last() {
+//             None => todo!(),
+//             Some(x) => x.block.clone(), // TODO: Make this more efficient.
+//         }
+//     }
+// }
--- rust/src/rule_system/validator.rs
+++ rust/src/rule_system/validator.rs
@@ -1,10 +1,11 @@
 // Library for validating rules.
 
 use std::iter::zip;
+use std::result::Result;
 
 use enum_map::enum_map;
 
-use crate::config::{Count, Rep, RepBlock, RepConfig, Tape};
+use crate::config::{Count, HalfTape, RepBlock, RepConfig, Tape};
 use crate::rule_system::algebra::{Expr, VarId, VarSubst};
 use crate::tm::Dir;
 
@@ -33,17 +34,31 @@ struct Rule {
 }
 
 // Try to match rule to this config and save variable assignments if it does match.
-fn try_match(config: &RepConfig<Expr>, rule: &Rule) -> Option<VarSubst> {
+// Currently we require the configs to have the exact same compression.
+fn try_match(config: &RepConfig<Expr>, rule: &Rule) -> Result<VarSubst, String> {
     if config.state != rule.init_config.state || config.dir != rule.init_config.dir {
-        return None;
+        return Err("State or direction do not match rule".to_string());
     }
     let mut assignment = VarSubst::new();
     for &dir in Dir::iter() {
-        if config.tape[dir].len() < rule.init_config.tape[dir].len() {
-            // Config is too small for rule. Rule cannot match config.
-            // TODO: Maybe deal with different representations of same config?
-            return None;
+        let rule_htape = &rule.init_config.tape[dir];
+        let config_htape = &config.tape[dir];
+        if rule_htape.is_complete {
+            // Complete rule
+            if !config_htape.is_complete {
+                return Err("Cannot apply complete rule to limited config".to_string());
+            }
+            if config_htape.data.len() != rule_htape.data.len() {
+                return Err("Complete config is not same length as complete rule".to_string());
+            }
+        } else {
+            // Limited rule
+            if config_htape.data.len() < rule_htape.data.len() {
+                return Err("Complete config is smaller than limited rule".to_string());
+            }
         }
+        // This zip is guaranteed to iterate over entire rule_htape b/c of the two
+        // htape.data.len() checks above.
         for (
             RepBlock {
                 block: c_block,
@@ -53,80 +68,81 @@ fn try_match(config: &RepConfig<Expr>, rule: &Rule) -> Option<VarSubst> {
                 block: r_block,
                 rep: r_rep,
             },
-        ) in zip(&config.tape[dir], &rule.init_config.tape[dir])
+        ) in zip(&config_htape.data, &rule_htape.data)
         {
             if c_block != r_block {
-                return None;
+                return Err("Block mismatch".to_string());
             }
             match r_rep {
-                Rep::Infinite => {
-                    if let Rep::Infinite = c_rep {
-                    } else {
-                        return None;
-                    }
-                }
-                Rep::Finite(RuleInitRep::Const(r_n)) => {
-                    if let Rep::Finite(Expr::Const(c_n)) = c_rep {
+                RuleInitRep::Const(r_n) => {
+                    if let Expr::Const(c_n) = c_rep {
                         if c_n != r_n {
-                            return None;
+                            return Err("Constant rep-count mismatch".to_string());
                         }
                     } else {
-                        // Constant in rule cannot match Inf or Expr on config.
-                        return None;
+                        return Err(
+                            "Constant rule rep-count cannot apply to variable config rep-count"
+                                .to_string(),
+                        );
                     }
                 }
-                Rep::Finite(RuleInitRep::Var { var, min: r_min }) => {
+                RuleInitRep::Var { var, min: r_min } => {
                     match c_rep {
-                        Rep::Finite(Expr::Const(c_n)) => {
+                        Expr::Const(c_n) => {
                             // Only apply rule if tape rep is >= min
                             if c_n < r_min {
-                                return None;
+                                return Err("Config rep-count below rule minimum".to_string());
                             }
                             assignment.insert(*var, Expr::Const(*c_n));
                         }
-                        Rep::Finite(_expr) => {
+                        _expr => {
                             todo!("Make sure expr cannot be below r_min!");
                             // assignment.insert(var, expr);
                         }
-                        Rep::Infinite => return None,
                     }
                 }
             }
         }
     }
-    Some(assignment)
-}
-
-fn update_rep(rep: &Rep<Expr>, subs: &VarSubst) -> Rep<Expr> {
-    match rep {
-        Rep::Infinite => Rep::Infinite,
-        Rep::Finite(expr) => {
-            // We should have assignments for all variables used in expr, so this
-            // should never fail (for valid rules).
-            Rep::Finite(expr.subst(subs).unwrap())
-        }
-    }
+    Ok(assignment)
 }
 
 fn update_rep_block(rep_block: &RepBlock<Expr>, subs: &VarSubst) -> RepBlock<Expr> {
     RepBlock {
         block: rep_block.block.clone(),
-        rep: update_rep(&rep_block.rep, subs),
+        rep: rep_block.rep.subst(subs).unwrap(),
+    }
+}
+
+fn update_half_tape_rep(half_tape: &HalfTape<Expr>, subs: &VarSubst) -> HalfTape<Expr> {
+    HalfTape {
+        data: half_tape
+            .data
+            .iter()
+            .map(|x| update_rep_block(x, subs))
+            .collect(),
+        is_complete: half_tape.is_complete,
     }
 }
 
 fn update_tape_rep(tape: &Tape<Expr>, subs: &VarSubst) -> Tape<Expr> {
     enum_map! {
-        Dir::Left => tape[Dir::Left].iter().map(|x| update_rep_block(x, subs)).collect(),
-        Dir::Right => tape[Dir::Right].iter().map(|x| update_rep_block(x, subs)).collect(),
+        Dir::Left => update_half_tape_rep(&tape[Dir::Left], subs),
+        Dir::Right => update_half_tape_rep(&tape[Dir::Right], subs),
     }
 }
 
-fn try_apply(config: &RepConfig<Expr>, rule: &Rule) -> Option<RepConfig<Expr>> {
+fn try_apply(config: &RepConfig<Expr>, rule: &Rule) -> Result<RepConfig<Expr>, String> {
     let subs = try_match(config, rule)?;
-    Some(RepConfig {
-        tape: update_tape_rep(&rule.final_config.tape, &subs),
-        state: rule.final_config.state,
-        dir: rule.final_config.dir,
-    })
+    if !rule.final_config.tape[Dir::Left].is_complete
+        || rule.final_config.tape[Dir::Right].is_complete
+    {
+        todo!("Handle limited configs correctly");
+    } else {
+        Ok(RepConfig {
+            tape: update_tape_rep(&rule.final_config.tape, &subs),
+            state: rule.final_config.state,
+            dir: rule.final_config.dir,
+        })
+    }
 }
