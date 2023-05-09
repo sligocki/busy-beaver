4c674049ad4d2dbb064cce93e4241ae072430135
Shawn Ligocki <sligocki@gmail.com>
2023-05-09 16:55:42 -0400

    Avoid copying holdouts if they are too big (>10GB).

--- Tools/sbatch_filter_setup.sh
+++ Tools/sbatch_filter_setup.sh
@@ -21,9 +21,11 @@ WORK_DIR=${ROOT_DIR}/${SUBDIR}/
 rm -rf ${WORK_DIR}
 mkdir -p ${WORK_DIR}/split/in/ ${WORK_DIR}/logs/
 
-# Pull in holdouts (from previous filter).
-cp ${ROOT_DIR}/unknown.txt ${WORK_DIR}/in.txt
+# Pull in holdouts from previous filter (skip this copy if holdouts file is too large).
+if [[ "$(wc -c < ${ROOT_DIR}/unknown.txt)" < 10000000000 ]]; then
+  cp ${ROOT_DIR}/unknown.txt ${WORK_DIR}/in.txt
+fi
 
 # Split inputs into shards so that array job can run.
 split --number=l/${NUM_SHARDS} --numeric-suffixes --suffix-length=8 \
-  ${WORK_DIR}/in.txt ${WORK_DIR}/split/in/in.
+  ${ROOT_DIR}/unknown.txt ${WORK_DIR}/split/in/in.
