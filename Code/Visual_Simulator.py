90fe066586db244f0aae834a69fe97b464852845
Shawn Ligocki <sligocki@gmail.com>
2025-06-25 15:07:01 -0400

    Support only printing in certain states.

--- Code/Visual_Simulator.py
+++ Code/Visual_Simulator.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python3
 # Visual simulation of a Turing Machine in terminal.
 
+from __future__ import annotations
+
 import argparse
+from dataclasses import dataclass
 import fcntl
 import re
 import string
@@ -9,7 +12,7 @@ import struct
 import sys
 import termios
 
-from Direct_Simulator import DirectSimulator
+from Direct_Simulator import DirectSimulator, State
 import IO
 from Macro import Turing_Machine
 
@@ -20,7 +23,44 @@ STATES = string.ascii_uppercase + string.ascii_lowercase + "!"
 COLOR = [49, 41, 42, 44, 46, 43, 45]
 
 
-def parse_config(config_str):
+@dataclass(frozen=True)
+class PrintFilter:
+  # Which states should we print for
+  states: list[State]
+  # Which locations should we print for
+  extreme: str
+
+  @staticmethod
+  def parse(filter_str: str) -> PrintFilter:
+    states = []
+    extreme = ""
+    for x in filter_str:
+      if x in "lr":
+        extreme += x
+      else:
+        states.append(STATES.index(x))
+    return PrintFilter(states, extreme)
+
+  def should_print(self, sim: DirectSimulator) -> bool:
+    return self.should_print_state(sim) and self.should_print_loc(sim)
+  
+  def should_print_state(self, sim: DirectSimulator) -> bool:
+    if not self.states:
+      return True
+    else:
+      return sim.state in self.states
+  
+  def should_print_loc(self, sim: DirectSimulator) -> bool:
+    if not self.extreme:
+      return True
+    if "l" in self.extreme and sim.tape.pos_leftmost() == sim.tape.position:
+      return True
+    if "r" in self.extreme and sim.tape.pos_rightmost() == sim.tape.position:
+      return True
+    return False
+
+
+def parse_config(config_str: str):
   left = []
   right = []
   in_left = True
@@ -60,47 +100,38 @@ def parse_config(config_str):
   return (state, left, right)
 
 
-def should_print(sim : DirectSimulator, print_ops : str) -> bool:
-  if "a" in print_ops:
-    return True
-  if "l" in print_ops and sim.tape.pos_leftmost() == sim.tape.position:
-    return True
-  if "r" in print_ops and sim.tape.pos_rightmost() == sim.tape.position:
-    return True
-  return False
-
 def print_tape(sim : DirectSimulator, args) -> None:
-  if should_print(sim, args.print):
-    half_width = args.print_width // 2 - 10
-    if args.relative:
-      print_range = range(sim.tape.position - half_width, sim.tape.position + half_width + 1)
+  half_width = args.print_width // 2 - 10
+  if args.relative:
+    print_range = range(sim.tape.position - half_width, sim.tape.position + half_width + 1)
+  else:
+    print_range = range(-half_width, half_width+1)
+
+  sys.stdout.write("\033[0m%10d: " % sim.step_num)
+
+  for pos in print_range:
+    value = sim.tape.read(pos)
+    if sim.tape.position == pos:
+      # If this is the current position ...
+      sys.stdout.write("\033[1;%dm%c" % (COLOR[value], STATES[sim.state]))
     else:
-      print_range = range(-half_width, half_width+1)
+      sys.stdout.write("\033[%dm " % COLOR[value])
 
-    sys.stdout.write("\033[0m%10d: " % sim.step_num)
+  state_str = sim.tm.states[sim.state] if not sim.halted else "HALT"
+  sys.stdout.write(f"\033[0m{state_str:4}")
+  sys.stdout.write(" \033[%dm%2d\033[0m\n" % (
+    COLOR[sim.tape.read()], sim.tape.read()))
 
-    for pos in print_range:
-      value = sim.tape.read(pos)
-      if sim.tape.position == pos:
-        # If this is the current possition ...
-        sys.stdout.write("\033[1;%dm%c" % (COLOR[value], STATES[sim.state]))
-      else:
-        sys.stdout.write("\033[%dm " % COLOR[value])
-
-    state_str = sim.tm.states[sim.state] if not sim.halted else "HALT"
-    sys.stdout.write(f"\033[0m{state_str:4}")
-    sys.stdout.write(" \033[%dm%2d\033[0m\n" % (
-      COLOR[sim.tape.read()], sim.tape.read()))
-
-    sys.stdout.flush()
+  sys.stdout.flush()
 
 
-def run_visual(sim : DirectSimulator, args):
+def run_visual(sim: DirectSimulator, filter: PrintFilter, args):
   print_tape(sim, args)
 
   while not sim.halted:
     sim.step()
-    print_tape(sim, args)
+    if filter.should_print(sim):
+      print_tape(sim, args)
 
   print("TM Halted on step", sim.step_num)
 
@@ -138,9 +169,10 @@ def main():
   # Print options
   parser.add_argument("--relative", "-r", action="store_true",
                       help="Print so that TM head always at the same column.")
-  parser.add_argument("--print", "-p", default="a",
-                      help="When to print tape: a: always, l: leftmost, r: rightmost. "
-                      "May be combined, ex: lr")
+  parser.add_argument("--print-filter", "-p", default="",
+                      help="When to print tape: l: leftmost, r: rightmost. "
+                      "Uppercase letters may be used for states as well. "
+                      "May be combined, ex: lrBC")
 
   args = parser.parse_args()
 
@@ -167,7 +199,9 @@ def main():
     # Write right half of tape so that the leftmost symbol is at location 0.
     sim.tape.write(symb, i)
 
-  run_visual(sim, args=args)
+  filter = PrintFilter.parse(args.print_filter)
+
+  run_visual(sim, filter, args=args)
   sys.stdout.flush()
 
 if __name__ == "__main__":
