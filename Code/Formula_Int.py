b492e1e654a8c58dc03ea0506d29308e7321dcd9
Shawn Ligocki <sligocki@gmail.com>
2025-07-16 00:18:42 -0400

    Switch Add to Sum to simplify expressions.

--- Code/Formula_Int.py
+++ Code/Formula_Int.py
@@ -9,36 +9,39 @@ from Math import carmichael
 
 type GenInt = FormulaInt | int
 
+def try_as_basic_int(n: GenInt) -> int | None:
+  if isinstance(n, (int, Int)):
+    return int(n)
+  return None
+
 class FormulaInt:
   """Abstract Base Class for Formula Integers."""
   def __add__(self, other: GenInt) -> FormulaInt:
-    return Add(self, other)
+    return Sum.new([self, other])
   def __radd__(self, other: GenInt) -> FormulaInt:
-    return Add(other, self)
+    return self + other
 
   def __sub__(self, other: GenInt) -> FormulaInt:
-    return Add(self, -other)
+    return self + (-other)
   def __rsub__(self, other: GenInt) -> FormulaInt:
-    return Add(other, -self)
+    return (-self) + other
 
   def __mul__(self, other: GenInt) -> FormulaInt:
     return Mul(self, other)
   def __rmul__(self, other: GenInt) -> FormulaInt:
-    return Mul(other, self)
+    return self * other
   
   def __neg__(self) -> FormulaInt:
-    return Mul(-1, self)
+    return -1 * self
 
   def __truediv__(self, denom: int) -> FormulaInt:
     return Div(self, denom)
-  
-  def __pow__(self, exponent: GenInt) -> FormulaInt:
-    return Pow(self, exponent)
+
   def __rpow__(self, base: int) -> FormulaInt:
     return Pow(base, self)
   
   def __mod__(self, modulus: int) -> int:
-    raise NotImplementedError
+    raise NotImplementedError(self)
   
   def __divmod__(self, other: int) -> tuple[FormulaInt, int]:
     m = self % other
@@ -49,16 +52,38 @@ class FormulaInt:
 class Int(FormulaInt):
   val: int
 
+  def __int__(self) -> int:
+    return self.val
+
   def __mod__(self, modulus: int) -> int:
     return self.val % modulus
 
 @dataclass(frozen=True)
-class Add(FormulaInt):
-  left: GenInt
-  right: GenInt
+class Sum(FormulaInt):
+  terms: list[FormulaInt]
+  const: int
+
+  @staticmethod
+  def new(vals: list[GenInt]) -> Sum:
+    terms: list[FormulaInt] = []
+    const = 0
+    for val in vals:
+      if (ival := try_as_basic_int(val)) != None:
+        const += ival
+      else:
+        terms.append(val)
+    return Sum(terms, const)
+  
+  def __add__(self, other: GenInt) -> FormulaInt:
+    terms = self.terms + [self.const]
+    if isinstance(other, Sum):
+      terms += other.terms + [other.const]
+    else:
+      terms.append(other)
+    return Sum.new(terms)
 
   def __mod__(self, modulus: int) -> int:
-    return ((self.left % modulus) + (self.right % modulus)) % modulus
+    return (sum(term % modulus for term in self.terms) + self.const) % modulus
 
 @dataclass(frozen=True)
 class Mul(FormulaInt):
