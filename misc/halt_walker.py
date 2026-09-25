3f1f2ebe73b4283d2880ca2d0619e78a2b8f4906
Shawn Ligocki <sligocki@gmail.com>
2026-09-25 12:26:24 -0400

    Overhaul script: 1. Add a bunch of new objects to encapsulate state: Config, StepResult, RunResult 2. Track runtime throughout system 3. Move init_runtime into arg since it depends on starting config.

--- misc/halt_walker.py
+++ misc/halt_walker.py
@@ -6,8 +6,11 @@
 #   * One Walk param (w)
 # which halt iff w < 0
 
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 import argparse
+from dataclasses import dataclass, replace
 import os
 import time
 
@@ -20,96 +23,148 @@ def process_memory() -> int:
   return psutil.Process(os.getpid()).memory_info().rss
 
 
-class HaltTransition(Exception):
-  """Raised by sim_step to indicate the machine halts at this step."""
-
-  def __init__(self, time_delta: int, config: list[int]):
-    self.time_delta = time_delta
-    self.config = config
-
-
-def print_info(i, start_h, w, max_w, h, t=0):
-  mod = 3**32
-  if h < mod:
-    h_str = f"{h:_}"
+def approx_str(n: int | None) -> str:
+  if n is None:
+    return ""
+  if n < 10**10:
+    return f"={n:_}"
   else:
-    h_str = f"~2^{h.bit_length():_} ≡ {h % mod} (mod 3^32)"
+    return f"≈2^{n.bit_length():_}"
 
-  if t:
-    t_str = f"t≈2^{t.bit_length():_}"
-  else:
-    t_str = ""
 
-  print(
-    f"  f^{{{i:_}}}({start_h:_}): w={w:_} max_w={max_w:_} h = {h_str} {t_str}  "
-    f"({process_memory() // 10**6:_}MB {time.process_time():_.0f}s)"
-  )
+@dataclass
+class Config:
+  is_halt: bool
+  h: mpz | None = None
+  w: int | None = None
+  halt_config: list[int] | None = None
+
+  @staticmethod
+  def running(h: mpz, w: int) -> Config:
+    return Config(is_halt=False, h=h, w=w)
+
+  @staticmethod
+  def halt(config: list[int]) -> Config:
+    # TODO: Make halt_config accurate
+    return Config(is_halt=True)  # , halt_config=config)
+
+  def __str__(self):
+    if self.is_halt:
+      # TODO: Add something about halt config
+      return "Halt"
+    else:
+      h_str = approx_str(self.h)
+      return f"w={self.w:_} h{h_str}"
+
+
+@dataclass
+class StepResult:
+  """Result of taking one sim steps."""
+
+  config: Config
+  delta_runtime: mpz
+
+
+@dataclass
+class RunResult:
+  """Result of taking one or more sim steps."""
+
+  config: Config
+  delta_sim_steps: int
+  delta_runtime: mpz
+  min_w: int
+  max_w: int
+
+  def concat(self, second: RunResult) -> RunResult:
+    """Merge two consecutive results"""
+    return RunResult(
+      config=second.config,
+      delta_sim_steps=self.delta_sim_steps + second.delta_sim_steps,
+      delta_runtime=self.delta_runtime + second.delta_runtime,
+      min_w=min(self.min_w, second.min_w),
+      max_w=max(self.max_w, second.max_w),
+    )
+
+  def __str__(self):
+    t_str = approx_str(self.delta_runtime)
+    return f"  sim_steps={self.delta_sim_steps:_}:  {self.config!s}  w_range=[{self.min_w:_}, {self.max_w:_}]  t{t_str}"
 
 
 class Sim(ABC):
   # Subclasses must define:
   #   mod_in: mpz
   #   mod_out: mpz
-  #   init_runtime: mpz
+  #   mod_time: mpz
 
   @abstractmethod
-  def sim_step(self, h: mpz, w: int) -> tuple[mpz, int, mpz]:
-    """Single simulation step: one application of high level function.
-    Must be implement by subclass.
-    Returns (new_h, new_w, delta_time)
-    Raises HaltTransition on halt
-    """
+  def sim_step(self, h: mpz, w: int) -> StepResult:
+    """Single simulation step: one application of high level function."""
 
-  def __init__(self, start_h: mpz, start_w: int):
-    self.start_h = start_h
-    self.h = start_h
-    self.w = start_w
-    self.max_w = start_w
-    self.num_sim_steps = 0
-    self.runtime = self.init_runtime
-    self.is_halted = False
+  def __init__(self, config: Config, init_runtime: mpz):
+    self.result = RunResult(
+      config=config,
+      delta_sim_steps=0,
+      delta_runtime=init_runtime,
+      min_w=config.w,
+      max_w=config.w,
+    )
+    # Copy
+    self.start_config = replace(config)
 
-  def direct(self, h: mpz, w: int, num_sim_steps: int):
-    """Directly simulate num_sim_steps sim_steps."""
-    min_w = 0
-    max_w = 0
+  def direct(self, config: Config, max_sim_steps: int) -> RunResult:
+    """Directly simulate for multiple steps (or until halt)"""
+    min_w: int = config.w
+    max_w: int = config.w
+    sim_steps = 0
     time = 0
-    for _ in range(num_sim_steps):
-      h, w, dt = self.sim_step(h, w)
-      time += dt
-      min_w = min(min_w, w)
-      max_w = max(max_w, w)
-    return (h, w, time, min_w, max_w)
+    while not config.is_halt and sim_steps < max_sim_steps:
+      # Update min/max before step since config.w is None after halt
+      min_w = min(min_w, config.w)
+      max_w = max(max_w, config.w)
 
-  def accel_pow(self, h: mpz, w: int, e: int):
-    """Accelerated sim of 2**e sim_steps. Uses divide-and-conquer."""
+      res = self.sim_step(config.h, config.w)
+
+      sim_steps += 1
+      time += res.delta_runtime
+      config = res.config
+
+    return RunResult(config=config, delta_sim_steps=sim_steps, delta_runtime=time, min_w=min_w, max_w=max_w)
+
+  def accel_pow(self, config: Config, e: int) -> RunResult:
+    """Accelerated sim of 2**e sim_steps. Uses divide-and-conquer.
+    Takes advantage of this identity:
+      f^n(x mod_in^n + r) = x mod_out^n + f^n(r)
+    """
+    assert not config.is_halt
     if e <= 6:
-      return self.direct(h, w, 2**e)
-    x = 1 << (e - 1)
-    p3t = self.mod_in**x
-    p4t = self.mod_out**x
+      return self.direct(config, 2**e)
+
+    def sim_expand(config: Config, e: int) -> RunResult:
+      max_sim_steps = 2**e
+      k, r = divmod(config.h, self.mod_in**max_sim_steps)
+      res = self.accel_pow(Config.running(r, config.w), e)
+
+      # Update h and runtime to account for extra (k) not accounted for in res
+      # Note: We only update for the actual number of sim steps executed (not max_sim_steps)
+      # TODO: How do we update halt config?
+      if res.config.h:
+        res.config.h += k * self.mod_out**res.delta_sim_steps
+      res.delta_runtime += k * self.mod_time**res.delta_sim_steps
+      return res
+
     # First half
-    k, r = divmod(h, p3t)
-    h1, w1, t1, lw1, hw1 = self.accel_pow(r, w, e - 1)
-    h1 += p4t * k
+    res1 = sim_expand(config, e - 1)
+    if res1.config.is_halt:
+      return res1
+
     # Second half
-    k1, r1 = divmod(h1, p3t)
-    h2, w2, t2, lw2, hw2 = self.accel_pow(r1, w1, e - 1)
-    h2 += p4t * k1
-    return (h2, w2, t1 + t2, min(lw1, lw2), max(hw1, hw2))
+    res2 = sim_expand(res1.config, e - 1)
+    return res1.concat(res2)
 
   def run_pow(self, e: int) -> None:
     """Run for 2^e sim_steps (or until halt if sooner). Updating self."""
-    try:
-      h, w, t, lw, hw = self.accel_pow(self.h, self.w, e)
-      self.h = h
-      self.w = w
-      self.max_w = max(self.max_w, hw)
-      self.num_sim_steps += 2**e
-      self.runtime += t
-    except HaltTransition as halt_info:
-      self.is_halted = True
-      self.runtime += halt_info.time_delta
+    res = self.accel_pow(self.result.config, e)
+    self.result = self.result.concat(res)
 
   def sim_forever(self, start_e: int = 0):
     """Simulate until halt (or memory failure)"""
@@ -117,38 +172,40 @@ class Sim(ABC):
     self.print_info()
     self.run_pow(start_e)
     e = start_e
-    while not self.is_halted:
+    while not self.result.config.is_halt:
       self.print_info()
       self.run_pow(e)
       e += 1
     print("Halt detected.")
+    self.print_info()
 
-  def sim_direct(self):
-    h, w = self.h, self.w
-    i = 0
-    next_i = 1
-    max_w = w
-    t = 0
-    while w >= 0:
-      try:
-        h, w, dt = self.sim_step(h, w)
-      except HaltTransition as halt_info:
-        i += 1
-        t += halt_info.time_delta
-        print("Halted via HaltTransition")
-        break
-      max_w = max(max_w, w)
-      i += 1
-      t += dt
-      if i == next_i:
-        print_info(i, self.start_h, w, max_w, h, t)
-        next_i *= 2
-    if w < 0:
-      print("Halted via w < 0")
-    print_info(i, self.start_h, w, max_w, h, t)
+  # TODO
+  # def sim_direct(self):
+  #   h, w = self.h, self.w
+  #   i = 0
+  #   next_i = 1
+  #   max_w = w
+  #   t = 0
+  #   while w >= 0:
+  #     try:
+  #       h, w, dt = self.sim_step(h, w)
+  #     except HaltTransition as halt_info:
+  #       i += 1
+  #       t += halt_info.time_delta
+  #       print("Halted via HaltTransition")
+  #       break
+  #     max_w = max(max_w, w)
+  #     i += 1
+  #     t += dt
+  #     if i == next_i:
+  #       print_info(i, self.start_h, w, max_w, h, t)
+  #       next_i *= 2
+  #   if w < 0:
+  #     print("Halted via w < 0")
+  #   print_info(i, self.start_h, w, max_w, h, t)
 
   def print_info(self) -> None:
-    print_info(self.num_sim_steps, self.start_h, self.w, self.max_w, self.h)
+    print(f"{self.result!s}  ({process_memory() // 10**6:_}MB {time.process_time():_.0f}s)")
 
 
 # Simulate MBB Cryptid: 0-BG_0-CF_0-DJ_1+E_1+A_2+J_2-H*_1-IE_0+J_0+H start @ state F
@@ -169,28 +226,31 @@ class Sim(ABC):
 class MBB1(Sim):
   mod_in = mpz(3)
   mod_out = mpz(4)
-  init_runtime = 3
+  mod_time = mpz(11)
 
-  def sim_step(self, h: mpz, w: int) -> tuple[mpz, int, mpz]:
+  def sim_step(self, h: mpz, w: int) -> StepResult:
     k, r = divmod(h, 3)
     if r == 0:
       if w == 0:
-        raise HaltTransition(time_delta=5 * k + 3, config=[0, 2 * k + 1, 0])
+        return StepResult(Config.halt([0, 2 * k + 1, 0]), 5 * k + 3)
       else:
-        return (4 * k + 2, w - 1, 11 * k + 7)
+        return StepResult(Config.running(4 * k + 2, w - 1), 11 * k + 7)
     elif r == 1:
-      return (4 * k + 3, w + 1, 11 * k + 9)
+      return StepResult(Config.running(4 * k + 3, w + 1), 11 * k + 9)
     else:
-      return (4 * k + 3, w, 11 * k + 9)
+      return StepResult(Config.running(4 * k + 3, w), 11 * k + 9)
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("start_value", type=int, nargs="?", default=1)
   parser.add_argument("start_offset", type=int, nargs="?", default=1)
+  parser.add_argument("init_runtime", type=int, nargs="?", default=3, help="TM steps until start config")
   args = parser.parse_args()
 
-  sim = MBB1(args.start_value, args.start_offset)
+  start_config = Config.running(args.start_value, args.start_offset)
+
+  sim = MBB1(start_config, args.init_runtime)
   sim.sim_forever()
   # print()
   # sim_direct(args.start_value, args.start_offset)
